d24b0c2e2e49884080cd4b4e80300e0bf6e14657
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-08-14 09:33:22 +0530

    reduce buffer sizes

--- sock_raw.c
+++ sock_raw.c
@@ -26,13 +26,13 @@ int main() {
   uint8_t *buf = aligned_alloc(4, 1600);
   struct iovec iov;
   iov.iov_base = buf;
-  iov.iov_len = 10000;
+  iov.iov_len = 1600;
   message_header.msg_iov = &iov;
   message_header.msg_iovlen = 1;
 
-  uint8_t control[10000];
+  uint8_t control[100];
   message_header.msg_control = &control;
-  message_header.msg_controllen = 10000;
+  message_header.msg_controllen = 100;
 
   while (1) {
     ssize_t res = recvmsg(raw_socket, &message_header, 0);
