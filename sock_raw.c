83e31a0b02ed534fa8dc89ab847f10c708a0e94f
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-08-14 09:34:17 +0530

    ignore packets sent to reserved addresses

--- sock_raw.c
+++ sock_raw.c
@@ -57,6 +57,45 @@ int main() {
       continue;
     }
 
+    // https://en.wikipedia.org/wiki/Reserved_IP_addresses
+    // ignore packets sent to
+    // 0.0.0.0/8
+    if ((dst_addr & 0xff000000) == 0x00000000 ||
+        // 10.0.0.0/8
+        (dst_addr & 0xff000000) == 0x0a000000 ||
+        // 100.64.0.0/10
+        (dst_addr & 0xffc00000) == 0x64400000 ||
+        // 127.0.0.0/8
+        (dst_addr & 0xff000000) == 0x7f000000 ||
+        // 169.254.0.0/16
+        (dst_addr & 0xffff0000) == 0xa9fe0000 ||
+        // 172.16.0.0/12
+        (dst_addr & 0xfff00000) == 0xac100000 ||
+        // 192.0.0.0/24
+        (dst_addr & 0xffffff00) == 0xc0000000 ||
+        // 192.0.2.0/24
+        (dst_addr & 0xffffff00) == 0xc0000200 ||
+        // 192.88.99.0/24
+        (dst_addr & 0xffffff00) == 0xc0586300 ||
+        // 192.168.0.0/16
+        (dst_addr & 0xffff0000) == 0xc0a80000 ||
+        // 198.18.0.0/15
+        (dst_addr & 0xfffe0000) == 0xc6120000 ||
+        // 198.51.100.0/24
+        (dst_addr & 0xffffff00) == 0xc6336400 ||
+        // 203.0.113.0/24
+        (dst_addr & 0xffffff00) == 0xcb007100 ||
+        // 224.0.0.0/4
+        (dst_addr & 0xf0000000) == 0xe0000000 ||
+        // 233.252.0.0/24
+        (dst_addr & 0xffffff00) == 0xe9fc0000 ||
+        // 240.0.0.0/4
+        (dst_addr & 0xf0000000) == 0xf0000000 ||
+        // 255.255.255.255/32
+        (dst_addr & 0xffffffff) == 0xffffffff) {
+      continue;
+    }
+
     printf("recvmsg %ld, ", res);
     for (ssize_t i = 0; i < res; i++) {
       printf("%02x", buf[i]);
