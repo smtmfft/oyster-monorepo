cc1d284de7483da5aeb6da14fa67c29ce1e17f61
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-08-15 16:49:01 +0530

    create and connect vsock socket

--- src/ip_to_vsock_raw.rs
+++ src/ip_to_vsock_raw.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use socket2::{Domain, SockAddr, Socket, Type};
+
+fn main() -> Result<()> {
+    let vsock_socket =
+        Socket::new(Domain::VSOCK, Type::STREAM, None).context("failed to create vsock socket")?;
+    vsock_socket
+        .connect(&SockAddr::vsock(3, 1200))
+        .context("failed to connect vsock socket")?;
+
+    Ok(())
 }
