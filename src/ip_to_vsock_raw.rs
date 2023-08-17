4f4beac4760473a2fef8215eb433422a67cb3687
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-08-17 11:21:21 +0530

    read from ip socket

--- src/ip_to_vsock_raw.rs
+++ src/ip_to_vsock_raw.rs
@@ -1,3 +1,5 @@
+use std::io::Read;
+
 use anyhow::{Context, Result};
 use socket2::{Domain, Protocol, SockAddr, Socket, Type};
 
@@ -8,11 +10,17 @@ fn main() -> Result<()> {
         .connect(&SockAddr::vsock(3, 1200))
         .context("failed to connect vsock socket")?;
 
-    let ip_socket = Socket::new(Domain::IPV4, Type::RAW, Protocol::TCP.into())
+    let mut ip_socket = Socket::new(Domain::IPV4, Type::RAW, Protocol::TCP.into())
         .context("failed to create ip socket")?;
     ip_socket
         .bind_device("lo".as_bytes().into())
         .context("failed to bind ip socket")?;
 
-    Ok(())
+    let mut buf = vec![0u8; 65536].into_boxed_slice();
+    loop {
+        // using read for now, investigate read_vectored for better perf
+        let size = ip_socket.read(&mut buf)?;
+
+        println!("{}, {:?}", size, &buf[0..size]);
+    }
 }
