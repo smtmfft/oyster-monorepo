d1f6fd89d314ec6807cef4036cbfce0e5da07a3f
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-09-27 13:06:08 +0530

    custom vsock address parser

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::ffi::OsStr;
 use std::pin::Pin;
 use std::task::{ready, Poll};
 
 use anyhow::Context;
 
 use axum::{routing::get, Router};
+use clap::{builder::TypedValueParser, error::ErrorKind, Arg, Command, Parser};
 use hyper::server::accept::Accept;
 use tokio_vsock::{VsockListener, VsockStream};
 
@@ -21,6 +23,37 @@ impl Accept for VsockServer {
     ) -> Poll<Option<Result<Self::Conn, Self::Error>>> {
         let (conn, _addr) = ready!(self.listener.poll_accept(cx))?;
         Poll::Ready(Some(Ok(conn)))
+    }
+}
+
+#[derive(Clone)]
+pub struct VsockAddrParser {}
+
+impl TypedValueParser for VsockAddrParser {
+    type Value = (u32, u32);
+
+    fn parse_ref(
+        &self,
+        cmd: &Command,
+        _: Option<&Arg>,
+        value: &OsStr,
+    ) -> Result<Self::Value, clap::Error> {
+        let value = value
+            .to_str()
+            .ok_or(clap::Error::new(ErrorKind::InvalidUtf8).with_cmd(cmd))?;
+
+        let (cid, port) = value
+            .split_once(':')
+            .ok_or(clap::Error::new(ErrorKind::ValueValidation).with_cmd(cmd))?;
+
+        let cid = cid
+            .parse::<u32>()
+            .map_err(|_| clap::Error::new(ErrorKind::ValueValidation).with_cmd(cmd))?;
+        let port = port
+            .parse::<u32>()
+            .map_err(|_| clap::Error::new(ErrorKind::ValueValidation).with_cmd(cmd))?;
+
+        Ok((cid, port))
     }
 }
 
