2ed1134efda66fa46e8ad52eb05eefc37ae5c3bf
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-09-27 12:58:07 +0530

    custom acceptor for vsock connections

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
+use std::pin::Pin;
+use std::task::{ready, Poll};
+
 use axum::{routing::get, Router};
+use hyper::server::accept::Accept;
+use tokio_vsock::{VsockListener, VsockStream};
+
+struct VsockServer {
+    listener: VsockListener,
+}
+
+impl Accept for VsockServer {
+    type Conn = VsockStream;
+    type Error = std::io::Error;
+
+    fn poll_accept(
+        mut self: Pin<&mut Self>,
+        cx: &mut std::task::Context,
+    ) -> Poll<Option<Result<Self::Conn, Self::Error>>> {
+        let (conn, _addr) = ready!(self.listener.poll_accept(cx))?;
+        Poll::Ready(Some(Ok(conn)))
+    }
+}
 
 #[tokio::main]
 async fn main() {
