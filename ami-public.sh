afd9795b2a9c3d93354233a3e0b326a6034ee292
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-10-16 10:15:17 +0530

    change visibility script to accept image family

--- ami-public.sh
+++ ami-public.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Params : <profile> <date> <region1> <region2> <region3>...
+# Params : <profile> <family> <date> <region1> <region2> <region3>...
 
 set -e
 
@@ -8,12 +8,12 @@ set -e
 export AWS_PROFILE=$1
 
 # List of regions where the amis will be made public
-regions=("${@:3}")
+regions=("${@:4}")
 
 # Copying both AMI's to each of the secified regions
 for r in ${regions[@]}; do
-    ami_amd=$(AWS_REGION=$r aws ec2 describe-images --owners self --filters "Name=name,Values=marlin/oyster/worker-amd64-$2" --no-paginate --query 'Images[0].ImageId' --output text)
-    ami_arm=$(AWS_REGION=$r aws ec2 describe-images --owners self --filters "Name=name,Values=marlin/oyster/worker-arm64-$2" --no-paginate --query 'Images[0].ImageId' --output text)
+    ami_amd=$(AWS_REGION=$r aws ec2 describe-images --owners self --filters "Name=name,Values=marlin/oyster/worker-$2-amd64-$3" --no-paginate --query 'Images[0].ImageId' --output text)
+    ami_arm=$(AWS_REGION=$r aws ec2 describe-images --owners self --filters "Name=name,Values=marlin/oyster/worker-$2-arm64-$3" --no-paginate --query 'Images[0].ImageId' --output text)
     if [[ $ami_amd != null && "$ami_amd" != "None" ]]; then
         echo "Making public amd64 image in region $r: $ami_amd"
         AWS_REGION=$r aws ec2 modify-image-attribute --image-id $ami_amd --launch-permission "Add=[{Group=all}]"
