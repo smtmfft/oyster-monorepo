42520fcaa0399e6245f019b91b653705f1f8c810
Roshan Raghupathy <thisisroshansmail@gmail.com>
2023-10-16 17:55:42 +0530

    change delete script to accept image family

--- ami-delete.sh
+++ ami-delete.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Params : <profile> <date> <region1> <region2> <region3>...
+# Params : <profile> <family> <date> <region1> <region2> <region3>...
 
 set -e
 
@@ -8,14 +8,14 @@ set -e
 export AWS_PROFILE=$1
 
 # List of regions the amis will be deleted from
-regions=("${@:3}")
+regions=("${@:4}")
 
 # Copying both AMI's to each of the secified regions
 for r in ${regions[@]}; do
-    ami_amd=$(AWS_REGION=$r aws ec2 describe-images --owners self --filters "Name=name,Values=marlin/oyster/worker-amd64-$2" --no-paginate --query 'Images[0].ImageId' --output text)
-    snap_amd=$(AWS_REGION=$r aws ec2 describe-images --owners self --filters "Name=name,Values=marlin/oyster/worker-amd64-$2" --no-paginate --query 'Images[0].BlockDeviceMappings[0].Ebs.SnapshotId' --output text)
-    ami_arm=$(AWS_REGION=$r aws ec2 describe-images --owners self --filters "Name=name,Values=marlin/oyster/worker-arm64-$2" --no-paginate --query 'Images[0].ImageId' --output text)
-    snap_arm=$(AWS_REGION=$r aws ec2 describe-images --owners self --filters "Name=name,Values=marlin/oyster/worker-arm64-$2" --no-paginate --query 'Images[0].BlockDeviceMappings[0].Ebs.SnapshotId' --output text)
+    ami_amd=$(AWS_REGION=$r aws ec2 describe-images --owners self --filters "Name=name,Values=marlin/oyster/worker-$2-amd64-$3" --no-paginate --query 'Images[0].ImageId' --output text)
+    snap_amd=$(AWS_REGION=$r aws ec2 describe-images --owners self --filters "Name=name,Values=marlin/oyster/worker-$2-amd64-$3" --no-paginate --query 'Images[0].BlockDeviceMappings[0].Ebs.SnapshotId' --output text)
+    ami_arm=$(AWS_REGION=$r aws ec2 describe-images --owners self --filters "Name=name,Values=marlin/oyster/worker-$2-arm64-$3" --no-paginate --query 'Images[0].ImageId' --output text)
+    snap_arm=$(AWS_REGION=$r aws ec2 describe-images --owners self --filters "Name=name,Values=marlin/oyster/worker-$2-arm64-$3" --no-paginate --query 'Images[0].BlockDeviceMappings[0].Ebs.SnapshotId' --output text)
     if [[ $ami_amd != null && "$ami_amd" != "None" ]]; then
         echo "Deleting amd64 image in region $r: $ami_amd"
         AWS_REGION=$r aws ec2 deregister-image --image-id $ami_amd
