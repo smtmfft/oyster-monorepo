70380803eb0033af54ae622db5ce8e390ebc13d0
Nitin Goel <nitingoel@Nitins-MacBook-Pro.local>
2023-11-24 17:33:28 +0530

    working and tested commit

--- src/handlers/attestationdoc.rs
+++ src/handlers/attestationdoc.rs
@@ -1,31 +1,30 @@
-use crate::config::Scep;
 use crate::types::handlers::AppState;
 use actix_web::{error, http::StatusCode, post, web, Responder};
 use derive_more::{Display, Error};
 use ethers;
 use hex;
-use libsodium_sys::crypto_sign;
 use libsodium_sys::crypto_sign_verify_detached;
 use oyster;
 use secp256k1;
 use serde::{Deserialize, Serialize};
-use serde_with::{serde_as, Bytes};
-use sha256::digest;
+use serde_with::serde_as;
 
-#[derive(Deserialize)]
+#[derive(Deserialize, Serialize)]
 struct VerifyAttestation {
-    attestation_doc: Vec<u8>,
+    attestation_doc: String,
     pcrs: Vec<String>,
     min_cpus: usize,
     min_mem: usize,
     max_age: usize,
-    signature: Vec<u8>,
+    signature: String,
+    secp_key: String,
 }
 
 #[serde_as]
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 struct VerifyAttestationResponse {
     sig: String,
+    secp_key: String,
 }
 
 #[derive(Debug, Display, Error)]
@@ -48,9 +47,8 @@ impl error::ResponseError for UserError {
 }
 
 fn abi_encode(
-    attestation: Vec<u8>,
-    source_pubkey: &[u8; 32],
-    enclave_pubkey: &[u8; 32],
+    prefix: String,
+    enclave_pubkey: &[u8; 65],
     pcr_0: Vec<u8>,
     pcr_1: Vec<u8>,
     pcr_2: Vec<u8>,
@@ -58,10 +56,7 @@ fn abi_encode(
     enclave_mem: usize,
 ) -> Vec<u8> {
     let mut encoded_data = Vec::new();
-    encoded_data.push(ethers::abi::Token::Bytes(attestation));
-    encoded_data.push(ethers::abi::Token::Address(address_from_pubkey(
-        source_pubkey,
-    )));
+    encoded_data.push(ethers::abi::Token::String(prefix));
     encoded_data.push(ethers::abi::Token::Address(address_from_pubkey(
         enclave_pubkey,
     )));
@@ -73,24 +68,27 @@ fn abi_encode(
     ethers::abi::encode(&encoded_data)
 }
 
-fn address_from_pubkey(pub_key: &[u8; 32]) -> ethers::types::Address {
-    let hash = ethers::utils::keccak256(pub_key);
+fn address_from_pubkey(pub_key: &[u8; 65]) -> ethers::types::Address {
+    let hash = ethers::utils::keccak256(&pub_key[1..]);
     ethers::types::Address::from_slice(&hash[12..])
 }
 
-fn verification_message(pubkey: &Vec<u8>) -> String {
+fn verification_message(pubkey: &String) -> String {
     const PREFIX: &str = "attestation-verification-";
     format!("{}{:?}", PREFIX.to_string(), pubkey)
 }
+
 #[post("/verify/attestation")]
 async fn verify(
-    attestation: web::Json<VerifyAttestation>,
+    req: web::Json<VerifyAttestation>,
     state: web::Data<AppState>,
 ) -> actix_web::Result<impl Responder, UserError> {
-    let msg = verification_message(&state.scep_public_key);
+    let msg = verification_message(&req.secp_key);
+    let sig_bytes = hex::decode(&req.signature).map_err(|_| UserError::InternalServerError)?;
+
     unsafe {
         let is_verified = crypto_sign_verify_detached(
-            attestation.signature.clone().as_mut_ptr(),
+            sig_bytes.clone().as_mut_ptr(),
             msg.as_ptr(),
             msg.len() as u64,
             state.enclave_public_key.as_ptr(),
@@ -99,68 +97,140 @@ async fn verify(
             return Err(UserError::InternalServerError);
         }
     }
-    let pub_key = oyster::verify(
-        attestation.attestation_doc.clone(),
-        attestation.pcrs.clone(),
-        attestation.min_cpus,
-        attestation.min_mem,
-        attestation.max_age,
+
+    let attestationdoc_bytes =
+        hex::decode(&req.attestation_doc).map_err(|_| UserError::InternalServerError)?;
+    let _ = oyster::verify(
+        attestationdoc_bytes,
+        req.pcrs.clone(),
+        req.min_cpus,
+        req.min_mem,
+        req.max_age,
     )
     .map_err(|_| UserError::InternalServerError)?;
 
+    let mut pubkey_bytes = [0u8; 65];
+    hex::decode_to_slice(&req.secp_key, &mut pubkey_bytes)
+        .map_err(|_| UserError::InternalServerError)?;
+
     let abi_encoded = abi_encode(
-        attestation.attestation_doc.clone(),
-        &pub_key,
-        &pub_key,
-        attestation.pcrs[0].clone().into_bytes(),
-        attestation.pcrs[1].clone().into_bytes(),
-        attestation.pcrs[2].clone().into_bytes(),
-        attestation.min_cpus,
-        attestation.min_mem,
+        "Enclave Attestation Verified".to_string(),
+        &pubkey_bytes,
+        hex::decode(req.pcrs[0].clone()).map_err(|_| UserError::InternalServerError)?,
+        hex::decode(req.pcrs[1].clone()).map_err(|_| UserError::InternalServerError)?,
+        hex::decode(req.pcrs[2].clone()).map_err(|_| UserError::InternalServerError)?,
+        req.min_cpus,
+        req.min_mem,
     );
 
-    const SIG_PREFIX: &str = "signed-attestation-verification-";
-    let msg_to_sign = format!("{}{}", SIG_PREFIX.to_string(), hex::encode(abi_encoded));
-    let msg_to_sign = digest(msg_to_sign);
-    let msg_to_sign = secp256k1::Message::from_digest_slice(msg_to_sign.as_bytes())
+    let msg_to_sign = ethers::utils::keccak256(abi_encoded);
+    let msg_to_sign = secp256k1::Message::from_digest_slice(&msg_to_sign)
         .map_err(|_| UserError::InternalServerError)?;
     let secp = secp256k1::Secp256k1::new();
-
     let sig = secp
-        .sign_ecdsa(&msg_to_sign, &state.scep_private_key)
-        .to_string();
-    Ok(web::Json(VerifyAttestationResponse { sig }))
+        .sign_ecdsa(&msg_to_sign, &state.secp_private_key)
+        .serialize_compact();
+    let sig = hex::encode(sig);
+    let sig = format!("{}1c", sig);
+    Ok(web::Json(VerifyAttestationResponse {
+        sig,
+        secp_key: hex::encode(state.secp_public_key),
+    }))
 }
 
 #[cfg(test)]
 
 mod tests {
     use super::*;
+    use actix_web::{test, web, App};
+    use libsodium_sys::crypto_sign_detached;
     use std::fs;
-    // #[actix_web::test]
-    // async fn test_attestation() {
-    //     println!("testing");
-    //     let attestation_doc = fs::read("./attestation_doc").unwrap();
-    //     let mut pcrs = Vec::new();
-    //     pcrs.push("3a2c64486fc890a1f65e82c195632b35a1b97d7595c666b8c83e91b56b92568abbeca0829269e40e4b76a6df963157da".to_string());
-    //     //pcrs.push("55ba3fa530581218580584144ce29c62c1c92f93c0bfcefead49c5fa174f15ba49a66a037957377abe34591364cbe935".to_string());
-    //     pcrs.push("be9dc8acb9b26e67f2919fe877f94271c79289989455013c66a5f2cc637a9355665bc9d89b7aed986f7b4c269acc1233".to_string());
-    //     pcrs.push("2cd79888cf800407c2bdd2165be71b8484561430942b314832cb11208ce774c757767893a84f52c46a41185f2248989f".to_string());
-    //     //pcrs.push("f064a1f5d2c0f49e3023a2f121c58ff5567ed423180da1f232f42093074b32f3e471b6bc946b9003e4725c9c2168ff25".to_string());
-    //     let result = oyster::verify(attestation_doc, pcrs, 2, 4134580224, 300000000).unwrap();
-    //     println!("publickey: {:?}", result);
-    // }
 
     #[actix_web::test]
-    async fn test_request_signature_verification() {
+    async fn test_handler() {
         let enclave_pub_key = fs::read("./enclave_public.key").unwrap();
         let enclave_priv_key = fs::read("./enclave_private.key").unwrap();
-        let scep_pub_key = fs::read("./public.key").unwrap();
+        let secp_priv_key = fs::read("./secret.key").unwrap();
+        let secp_priv_key = secp256k1::SecretKey::from_slice(&secp_priv_key).unwrap();
+        let secp = secp256k1::Secp256k1::new();
 
-        let msg_to_sign = verification_message(&scep_pub_key);
+        let secp_pub_key = secp_priv_key.public_key(&secp).serialize_uncompressed();
+        println!("address : {}", address_from_pubkey(&secp_pub_key));
+        let msg_to_sign = verification_message(&hex::encode(&secp_pub_key));
         let mut sig = [0u8; 64];
         unsafe {
-            let is_signed = crypto_sign(
+            let is_signed = crypto_sign_detached(
+                sig.as_mut_ptr(),
+                std::ptr::null_mut(),
+                msg_to_sign.as_ptr(),
+                msg_to_sign.len() as u64,
+                enclave_priv_key.as_ptr(),
+            );
+            if is_signed != 0 {
+                panic!("not signed");
+            }
+        }
+
+        let app = test::init_service(
+            App::new()
+                .app_data(web::Data::new(AppState {
+                    enclave_public_key: enclave_pub_key.clone(),
+                    secp_private_key: secp_priv_key.clone(),
+                    secp_public_key: secp_pub_key.clone(),
+                }))
+                .service(verify),
+        )
+        .await;
+        let mut pcrs = Vec::new();
+        pcrs.push("3a2c64486fc890a1f65e82c195632b35a1b97d7595c666b8c83e91b56b92568abbeca0829269e40e4b76a6df963157da".to_string());
+        pcrs.push("be9dc8acb9b26e67f2919fe877f94271c79289989455013c66a5f2cc637a9355665bc9d89b7aed986f7b4c269acc1233".to_string());
+        pcrs.push("2cd79888cf800407c2bdd2165be71b8484561430942b314832cb11208ce774c757767893a84f52c46a41185f2248989f".to_string());
+
+        let req_data = VerifyAttestation {
+            attestation_doc: hex::encode(fs::read("./attestation_doc").unwrap()),
+            pcrs,
+            min_cpus: 2,
+            min_mem: 4134580224,
+            max_age: 300000000,
+            signature: hex::encode(sig),
+            secp_key: hex::encode(&secp_pub_key).clone(),
+        };
+        let req = test::TestRequest::post()
+            .uri("/verify/attestation")
+            .set_json(req_data)
+            .to_request();
+
+        let resp: VerifyAttestationResponse = test::call_and_read_body_json(&app, req).await;
+
+        println!("resp sig: {}", resp.sig);
+        println!("resp secpkey: {}", resp.secp_key);
+    }
+
+    #[actix_web::test]
+    async fn test_attestation() {
+        println!("testing");
+        let attestation_doc = fs::read("./attestation_doc").unwrap();
+        let mut pcrs = Vec::new();
+        pcrs.push("3a2c64486fc890a1f65e82c195632b35a1b97d7595c666b8c83e91b56b92568abbeca0829269e40e4b76a6df963157da".to_string());
+
+        pcrs.push("be9dc8acb9b26e67f2919fe877f94271c79289989455013c66a5f2cc637a9355665bc9d89b7aed986f7b4c269acc1233".to_string());
+        pcrs.push("2cd79888cf800407c2bdd2165be71b8484561430942b314832cb11208ce774c757767893a84f52c46a41185f2248989f".to_string());
+        let result = oyster::verify(attestation_doc, pcrs, 2, 4134580224, 300000000).unwrap();
+        println!("publickey: {:?}", result);
+    }
+
+    #[actix_web::test]
+    async fn test_signature_verification() {
+        let enclave_pub_key = fs::read("./enclave_public.key").unwrap();
+        let enclave_priv_key = fs::read("./enclave_private.key").unwrap();
+        let secp_priv_key = fs::read("./secret.key").unwrap();
+        let secp_priv_key = secp256k1::SecretKey::from_slice(&secp_priv_key).unwrap();
+        let secp = secp256k1::Secp256k1::new();
+        let secp_pub_key = secp_priv_key.public_key(&secp).serialize_uncompressed();
+        let msg_to_sign = verification_message(&hex::encode(secp_pub_key));
+        let mut sig = [0u8; 64];
+        unsafe {
+            let is_signed = crypto_sign_detached(
                 sig.as_mut_ptr(),
                 std::ptr::null_mut(),
                 msg_to_sign.as_ptr(),
@@ -183,19 +253,5 @@ mod tests {
                 panic!("not verified");
             }
         }
-    }
-
-    #[actix_web::test]
-    async fn test_signature_generation() {
-        println!("testing");
-        let attestation_doc = fs::read("./attestation_doc").unwrap();
-        let mut pcrs = Vec::new();
-        pcrs.push("3a2c64486fc890a1f65e82c195632b35a1b97d7595c666b8c83e91b56b92568abbeca0829269e40e4b76a6df963157da".to_string());
-        //pcrs.push("55ba3fa530581218580584144ce29c62c1c92f93c0bfcefead49c5fa174f15ba49a66a037957377abe34591364cbe935".to_string());
-        pcrs.push("be9dc8acb9b26e67f2919fe877f94271c79289989455013c66a5f2cc637a9355665bc9d89b7aed986f7b4c269acc1233".to_string());
-        pcrs.push("2cd79888cf800407c2bdd2165be71b8484561430942b314832cb11208ce774c757767893a84f52c46a41185f2248989f".to_string());
-        //pcrs.push("f064a1f5d2c0f49e3023a2f121c58ff5567ed423180da1f232f42093074b32f3e471b6bc946b9003e4725c9c2168ff25".to_string());
-        let result = oyster::verify(attestation_doc, pcrs, 2, 4134580224, 300000000).unwrap();
-        println!("publickey: {:?}", result);
     }
 }
