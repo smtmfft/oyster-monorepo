8b9304004f9e84284f66503dd12d49689a58ae6e
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-01-15 20:06:35 +0530

    secp256k1_private_key -> secp256k1_secret

--- src/main.rs
+++ src/main.rs
@@ -26,22 +26,22 @@ struct Cli {
 #[actix_web::main]
 async fn main() -> Result<()> {
     let cli = Cli::parse();
-    let secp256k1_private_key = fs::read(cli.secp256k1_secret.clone()).with_context(|| {
+    let secp256k1_secret = fs::read(cli.secp256k1_secret.clone()).with_context(|| {
         format!(
             "Failed to read secp256k1_private from {}",
             cli.secp256k1_secret
         )
     })?;
-    let secp256k1_private_key = secp256k1::SecretKey::from_slice(&secp256k1_private_key)
+    let secp256k1_secret = secp256k1::SecretKey::from_slice(&secp256k1_secret)
         .context("unable to decode secp256k1_private key from slice")?;
     let secp256k1 = secp256k1::Secp256k1::new();
-    let secp256k1_public_key = secp256k1_private_key
+    let secp256k1_public_key = secp256k1_secret
         .public_key(&secp256k1)
         .serialize_uncompressed();
     let server = HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(AppState {
-                secp256k1_secret: secp256k1_private_key.clone(),
+                secp256k1_secret: secp256k1_secret.clone(),
                 secp256k1_public_key,
             }))
             .service(handler::verify)
