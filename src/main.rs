9e923d1a0fbec3efbef7fa9f41c96519342b3012
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-01-15 20:20:48 +0530

    read public key from file instead of computing

--- src/main.rs
+++ src/main.rs
@@ -30,18 +30,27 @@ struct Cli {
 #[actix_web::main]
 async fn main() -> Result<()> {
     let cli = Cli::parse();
+
     let secp256k1_secret = fs::read(cli.secp256k1_secret.clone()).with_context(|| {
         format!(
-            "Failed to read secp256k1_private from {}",
+            "Failed to read secp256k1_secret from {}",
             cli.secp256k1_secret
         )
     })?;
     let secp256k1_secret = secp256k1::SecretKey::from_slice(&secp256k1_secret)
-        .context("unable to decode secp256k1_private key from slice")?;
-    let secp256k1 = secp256k1::Secp256k1::new();
-    let secp256k1_public = secp256k1_secret
-        .public_key(&secp256k1)
-        .serialize_uncompressed();
+        .context("unable to decode secp256k1_secret key from slice")?;
+
+    let secp256k1_public = fs::read(cli.secp256k1_public.clone()).with_context(|| {
+        format!(
+            "Failed to read secp256k1_public from {}",
+            cli.secp256k1_public
+        )
+    })?;
+    let secp256k1_public: [u8; 65] = secp256k1_public
+        .as_slice()
+        .try_into()
+        .context("invalid public key length")?;
+
     let server = HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(AppState {
