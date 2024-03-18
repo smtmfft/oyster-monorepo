436108241ad4afdf2d7e17338761c481ef375376
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-03-18 17:12:37 +0530

    add a status subcommand

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ mod utils;
 use aws_config::SdkConfig;
 use aws_types::region::Region;
 use chrono::Local;
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use tokio::time::{interval, Duration};
 
 #[derive(Parser, Clone)]
@@ -57,6 +57,24 @@ struct Cli {
         default_value = "us-east-1,us-east-2,us-west-1,us-west-2,ca-central-1,sa-east-1,eu-north-1,eu-west-3,eu-west-2,eu-west-1,eu-central-1,eu-central-2,eu-south-1,eu-south-2,me-south-1,me-central-1,af-south-1,ap-south-1,ap-south-2,ap-northeast-1,ap-northeast-2,ap-northeast-3,ap-southeast-1,ap-southeast-2,ap-southeast-3,ap-southeast-4,ap-east-1"
     )]
     aws_regions: String,
+
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    // get status of a specific quota in a specific region
+    Status {
+        #[clap(long, value_parser)]
+        quota: String,
+
+        #[clap(long, value_parser)]
+        region: String,
+
+        #[clap(long, value_parser)]
+        profile: String,
+    },
 }
 
 async fn limit_status(quota_name: &str, config: &SdkConfig) {
