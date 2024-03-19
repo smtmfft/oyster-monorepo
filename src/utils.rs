e18f981b429ebac4648ea600123bac53ef2feedd
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-03-19 16:01:46 +0530

    add quota struct

--- src/utils.rs
+++ src/utils.rs
@@ -2,6 +2,21 @@ pub const EC2_SERVICE_CODE: &str = "ec2";
 pub const VCPU_QUOTA_NAME: &str = "vcpu";
 pub const ELASTIC_IP_QUOTA_NAME: &str = "eip";
 
+pub enum Quota {
+    Vcpu,
+    Eip,
+}
+
+impl Quota {
+    pub fn from_name(name: &str) -> anyhow::Result<Quota> {
+        match name {
+            "vcpus" => Ok(Quota::Vcpu),
+            "eips" => Ok(Quota::Eip),
+            _ => Err(anyhow::anyhow!("invalid quota, should be vcpus or eips")),
+        }
+    }
+}
+
 pub fn map_quota_to_code(quota_name: &str) -> Option<String> {
     match quota_name {
         VCPU_QUOTA_NAME => Some(String::from("L-1216C47A")),
