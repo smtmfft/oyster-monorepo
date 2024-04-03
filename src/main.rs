b558a87e035a8db80932c6366e38666b795ef359
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-04-03 19:21:02 +0530

    requests command

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod scheduled_tasks;
 mod service_quotas;
 mod utils;
 
-use anyhow::{Context, Result};
+use anyhow::{anyhow, Context, Result};
 use aws_types::region::Region;
 use clap::{Parser, Subcommand};
 
@@ -18,6 +18,23 @@ struct Cli {
 enum Commands {
     // get usage and limit of specific quotas in specific regions
     Status {
+        #[clap(long, value_parser = utils::Quota::from_name, num_args = 1.., value_delimiter = ',', default_value = "vcpus,eips")]
+        quotas: Vec<utils::Quota>,
+
+        #[clap(
+            long,
+            value_parser,
+            num_args = 1..,
+            value_delimiter = ',',
+            default_value = "us-east-1,us-east-2,us-west-1,us-west-2,ca-central-1,sa-east-1,eu-north-1,eu-west-3,eu-west-2,eu-west-1,eu-central-1,eu-central-2,eu-south-1,eu-south-2,me-south-1,me-central-1,af-south-1,ap-south-1,ap-south-2,ap-northeast-1,ap-northeast-2,ap-northeast-3,ap-southeast-1,ap-southeast-2,ap-southeast-3,ap-southeast-4,ap-east-1",
+        )]
+        regions: Vec<String>,
+
+        #[clap(long, value_parser)]
+        profile: String,
+    },
+    // get statuses of requests of specific quotas in specific regions
+    Requests {
         #[clap(long, value_parser = utils::Quota::from_name, num_args = 1.., value_delimiter = ',', default_value = "vcpus,eips")]
         quotas: Vec<utils::Quota>,
 
@@ -244,6 +261,17 @@ async fn main() -> Result<()> {
             for region in regions {
                 for quota in quotas.as_slice() {
                     quota_status(quota, &region, &profile).await?;
+                }
+            }
+        }
+        Commands::Requests {
+            quotas,
+            regions,
+            profile,
+        } => {
+            for region in regions {
+                for quota in quotas.as_slice() {
+                    request_status(quota, &region, &profile).await?;
                 }
             }
         }
