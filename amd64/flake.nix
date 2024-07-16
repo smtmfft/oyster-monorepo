463ee7e1ebc0c13ce1901ec3f407f3cc39a96d96
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-07-16 15:09:21 +0530

    provisioning the app directory

--- amd64/flake.nix
+++ amd64/flake.nix
@@ -9,9 +9,19 @@
 		nitro = nitro-util.lib.${system};
 		eifArch = "x86_64";
 		pkgs = nixpkgs.legacyPackages."${system}";
+		supervisord = pkgs.fetchurl {
+			url = "http://public.artifacts.marlin.pro/projects/enclaves/supervisord_c2cae38b_linux_amd64";
+			sha256 = "46bf15be56a4cac3787f3118d5b657187ee3e4d0a36f3aa2970f3ad3bd9f2712";
+		};
 		in {
+			app = pkgs.runCommand "app" {} ''
+			echo Preparing the app folder
+			mkdir -p $out
+			mkdir -p $out/app
+			cp ${supervisord} $out/app/supervisord
+			'';
 			packages.${system}.default = nitro.buildEif {
-				name = "enclave.eif";
+				name = "enclave";
 				arch = eifArch;
 
 				# use AWS' nitro-cli binary blobs
@@ -21,8 +31,8 @@
 				env = "";
 				copyToRoot = pkgs.buildEnv {
 					name = "image-root";
-					paths = [ pkgs.hello ];
-					pathsToLink = [ "/bin" ];
+					paths = [ pkgs.hello self.app ];
+					pathsToLink = [ "/bin" "/app" ];
 				};
 			};
 		};
