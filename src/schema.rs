1931c534e38271d3f2539b14cfeb39c494d125d2
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-11 15:13:18 +0530

    generate schema

--- src/schema.rs
+++ src/schema.rs
@@ -1,2 +1,10 @@
 // @generated automatically by Diesel CLI.
 
+diesel::table! {
+    providers (id) {
+        #[max_length = 42]
+        id -> Bpchar,
+        cp -> Text,
+        is_active -> Bool,
+    }
+}
