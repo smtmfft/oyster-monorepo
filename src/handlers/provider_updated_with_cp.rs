b91ee0e26fccffc30b5d1be9859fd0143cce59a9
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-12 20:28:24 +0530

    split off ProviderUpdatedWithCp

---
+++ src/handlers/provider_updated_with_cp.rs
@@ -0,0 +1,35 @@
+use crate::schema::providers;
+use alloy::primitives::Address;
+use alloy::rpc::types::Log;
+use alloy::sol_types::SolValue;
+use anyhow::Context;
+use anyhow::Result;
+use diesel::ExpressionMethods;
+use diesel::PgConnection;
+use diesel::RunQueryDsl;
+use tracing::warn;
+use tracing::{info, instrument};
+
+#[instrument(level = "info", skip_all, parent = None, fields(block = log.block_number, idx = log.log_index))]
+pub fn handle_provider_updated_with_cp(conn: &mut PgConnection, log: Log) -> Result<()> {
+    info!(?log, "processing");
+
+    let provider = Address::from_word(log.topics()[1]).to_checksum(None);
+    let cp = String::abi_decode(&log.data().data, true)?;
+
+    info!(provider, "updating provider");
+    let count = diesel::update(providers::table)
+        .filter(providers::id.eq(&provider))
+        .set(providers::cp.eq(cp))
+        .execute(conn)
+        .context("failed to update provider")?;
+
+    // warn just in case
+    if count != 1 {
+        warn!(count, "count should have been 1");
+    }
+
+    info!(provider, "updated provider");
+
+    Ok(())
+}
