a20e8f7b425f58d97c0594aa35ddb19aa0ca7329
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-12 19:50:19 +0530

    add test that removes an existing provider

--- src/handlers/provider_removed.rs
+++ src/handlers/provider_removed.rs
@@ -37,3 +37,84 @@ pub fn handle_provider_removed(conn: &mut PgConnection, log: Log) -> Result<()> 
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use alloy::primitives::Bytes;
+    use alloy::{primitives::LogData, rpc::types::Log};
+    use anyhow::Result;
+    use diesel::QueryDsl;
+    use ethp::{event, keccak256};
+
+    use crate::handlers::handle_log;
+    use crate::handlers::test_db::TestDb;
+
+    use super::*;
+
+    #[test]
+    fn test_remove_existing_provider() -> Result<()> {
+        // setup
+        let mut db = TestDb::new();
+        let conn = &mut db.conn;
+
+        let contract = "0x1111111111111111111111111111111111111111".parse()?;
+
+        diesel::insert_into(providers::table)
+            .values((
+                providers::id.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                providers::cp.eq("some other cp"),
+                providers::is_active.eq(true),
+            ))
+            .execute(conn)?;
+
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some other cp".to_owned(),
+                true
+            ))
+        );
+
+        // log under test
+        let log = Log {
+            block_hash: Some(keccak256!("some block").into()),
+            block_number: Some(42),
+            block_timestamp: None,
+            log_index: Some(69),
+            transaction_hash: Some(keccak256!("some tx").into()),
+            transaction_index: Some(420),
+            removed: false,
+            inner: alloy::primitives::Log {
+                address: contract,
+                data: LogData::new(
+                    vec![
+                        event!("ProviderRemoved(address)").into(),
+                        "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"
+                            .parse::<Address>()?
+                            .into_word(),
+                    ],
+                    Bytes::new(),
+                )
+                .unwrap(),
+            },
+        };
+
+        // use handle_log instead of concrete handler to test dispatch
+        handle_log(conn, log)?;
+
+        // checks
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some cp".to_owned(),
+                false
+            ))
+        );
+
+        Ok(())
+    }
+}
