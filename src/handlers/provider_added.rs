d3344c6c27acb3a5ed41f413a2e7e2a04d121624
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-12 17:06:06 +0530

    add basic test

--- src/handlers/provider_added.rs
+++ src/handlers/provider_added.rs
@@ -30,3 +30,65 @@ pub fn handle_provider_added(conn: &mut PgConnection, log: Log) -> Result<()> {
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use alloy::{primitives::LogData, rpc::types::Log};
+    use anyhow::Result;
+    use diesel::QueryDsl;
+    use ethp::{event, keccak256};
+
+    use crate::handlers::handle_log;
+    use crate::handlers::test_db::TestDb;
+
+    use super::*;
+
+    #[test]
+    fn test_add_new_provider_in_empty_db() -> Result<()> {
+        // setup
+        let mut db = TestDb::new();
+        let conn = &mut db.conn;
+
+        let contract = "0x1111111111111111111111111111111111111111".parse()?;
+
+        // log under test
+        let log = Log {
+            block_hash: Some(keccak256!("some block").into()),
+            block_number: Some(42),
+            block_timestamp: None,
+            log_index: Some(69),
+            transaction_hash: Some(keccak256!("some tx").into()),
+            transaction_index: Some(420),
+            removed: false,
+            inner: alloy::primitives::Log {
+                address: contract,
+                data: LogData::new(
+                    vec![
+                        event!("ProviderAdded(address,string)").into(),
+                        "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"
+                            .parse::<Address>()?
+                            .into_word(),
+                    ],
+                    "some cp".abi_encode().into(),
+                )
+                .unwrap(),
+            },
+        };
+
+        // use handle_log instead of concrete handler to test dispatch
+        handle_log(conn, log)?;
+
+        // checks
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some cp".to_owned(),
+                true
+            ))
+        );
+
+        Ok(())
+    }
+}
