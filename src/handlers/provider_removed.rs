a8d3a2fb4c0bc98a63a4f83c46b168c4eade1aab
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-12 19:50:44 +0530

    add test that tries to remove non existent provider

--- src/handlers/provider_removed.rs
+++ src/handlers/provider_removed.rs
@@ -117,4 +117,50 @@ mod tests {
 
         Ok(())
     }
+
+    #[test]
+    fn test_remove_nonexistent_provider() -> Result<()> {
+        // setup
+        let mut db = TestDb::new();
+        let conn = &mut db.conn;
+
+        let contract = "0x1111111111111111111111111111111111111111".parse()?;
+
+        assert_eq!(providers::table.count().get_result(conn), Ok(0));
+
+        // log under test
+        let log = Log {
+            block_hash: Some(keccak256!("some block").into()),
+            block_number: Some(42),
+            block_timestamp: None,
+            log_index: Some(69),
+            transaction_hash: Some(keccak256!("some tx").into()),
+            transaction_index: Some(420),
+            removed: false,
+            inner: alloy::primitives::Log {
+                address: contract,
+                data: LogData::new(
+                    vec![
+                        event!("ProviderRemoved(address)").into(),
+                        "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"
+                            .parse::<Address>()?
+                            .into_word(),
+                    ],
+                    Bytes::new(),
+                )
+                .unwrap(),
+            },
+        };
+
+        // use handle_log instead of concrete handler to test dispatch
+        let res = handle_log(conn, log);
+
+        // checks
+        assert_eq!(
+            format!("{:?}", res.unwrap_err()),
+            "count 0 should have been 1"
+        );
+
+        Ok(())
+    }
 }
