ab6f10fb6cf872cecdd26ed73e1e4b8683ac78ff
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-12 13:16:39 +0530

    add tx hash to log

--- src/handlers.rs
+++ src/handlers.rs
@@ -2,7 +2,12 @@ use alloy::rpc::types::Log;
 use anyhow::Result;
 use tracing::{info, instrument};
 
-#[instrument(level = "info", skip_all, parent = None, fields(block = log.block_number, idx = log.log_index))]
+#[instrument(
+    level = "info",
+    skip_all,
+    parent = None,
+    fields(block = log.block_number, idx = log.log_index, tx = ?log.transaction_hash
+))]
 pub fn handle_log(log: Log) -> Result<()> {
     info!(?log, "processing");
     Ok(())
