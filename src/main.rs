81938681e4c5d44a196f7bfae566746fdf1693a3
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-12 11:37:30 +0530

    use start_from to set start block of contract deployment

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use diesel::PgConnection;
 use dotenvy::dotenv;
 
 use oyster_indexer::event_loop;
+use oyster_indexer::start_from;
 use oyster_indexer::AlloyProvider;
 
 fn main() -> Result<()> {
@@ -14,12 +15,12 @@ fn main() -> Result<()> {
     let conn = PgConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
 
+    let mut conn = oyster_indexer::AnyConnection::Postgresql(conn);
     let provider = AlloyProvider {
         url: "https://arb1.arbitrum.io/rpc".parse()?,
         contract: "0x9d95D61eA056721E358BC49fE995caBF3B86A34B".parse()?,
     };
-    event_loop(
-        &mut oyster_indexer::AnyConnection::Postgresql(conn),
-        provider,
-    )
+    let is_start_set = start_from(&mut conn, 87252070)?;
+    println!("is_start_set: {}", is_start_set);
+    event_loop(&mut conn, provider)
 }
