639e5dd60148e235a5bb803416d50d81b58057f8
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-12 13:25:37 +0530

    ignore certain logs

--- src/handlers.rs
+++ src/handlers.rs
@@ -7,6 +7,14 @@ use tracing::{info, instrument};
 
 static PROVIDER_ADDED_TOPIC: [u8; 32] = event!("ProviderAdded(address,string)");
 
+// Ignored logs
+static UPGRADED_TOPIC: [u8; 32] = event!("Upgraded(address)");
+static LOCK_WAIT_TIME_UPDATED_TOPIC: [u8; 32] =
+    event!("LockWaitTimeUpdated(bytes32,uint256,uint256)");
+static ROLE_GRANTED_TOPIC: [u8; 32] = event!("RoleGranted(bytes32,address,address)");
+static TOKEN_UPDATED_TOPIC: [u8; 32] = event!("TokenUpdated(address,address)");
+static INITIALIZED_TOPIC: [u8; 32] = event!("Initialized(uint8)");
+
 #[instrument(
     level = "info",
     skip_all,
@@ -22,6 +30,14 @@ pub fn handle_log(log: Log) -> Result<()> {
 
     if log_type == PROVIDER_ADDED_TOPIC {
         handle_provider_added(log)
+    } else if log_type == UPGRADED_TOPIC
+        || log_type == LOCK_WAIT_TIME_UPDATED_TOPIC
+        || log_type == ROLE_GRANTED_TOPIC
+        || log_type == TOKEN_UPDATED_TOPIC
+        || log_type == INITIALIZED_TOPIC
+    {
+        info!(?log_type, "ignoring log type");
+        Ok(())
     } else {
         warn!(?log_type, "unknown log type");
         Ok(())
@@ -33,3 +49,7 @@ pub fn handle_provider_added(log: Log) -> Result<()> {
     info!(?log, "processing");
     Ok(())
 }
+
+// log ProviderAdded(address indexed provider, string cp);
+// event ProviderRemoved(address indexed provider);
+// event ProviderUpdatedWithCp(address indexed provider, string newCp);
