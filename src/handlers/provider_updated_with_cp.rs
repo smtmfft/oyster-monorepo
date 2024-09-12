b81c45d9514c35655e112f88e6e41e0d2a147652
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-12 20:43:37 +0530

    add test that tries to change cp of an inactive provider

--- src/handlers/provider_updated_with_cp.rs
+++ src/handlers/provider_updated_with_cp.rs
@@ -230,4 +230,66 @@ mod tests {
 
         Ok(())
     }
+
+    #[test]
+    fn test_change_cp_of_inactive_provider() -> Result<()> {
+        // setup
+        let mut db = TestDb::new();
+        let conn = &mut db.conn;
+
+        let contract = "0x1111111111111111111111111111111111111111".parse()?;
+
+        diesel::insert_into(providers::table)
+            .values((
+                providers::id.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                providers::cp.eq("some cp"),
+                providers::is_active.eq(false),
+            ))
+            .execute(conn)?;
+
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some cp".to_owned(),
+                false
+            ))
+        );
+
+        // log under test
+        let log = Log {
+            block_hash: Some(keccak256!("some block").into()),
+            block_number: Some(42),
+            block_timestamp: None,
+            log_index: Some(69),
+            transaction_hash: Some(keccak256!("some tx").into()),
+            transaction_index: Some(420),
+            removed: false,
+            inner: alloy::primitives::Log {
+                address: contract,
+                data: LogData::new(
+                    vec![
+                        event!("ProviderUpdatedWithCp(address,string)").into(),
+                        "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"
+                            .parse::<Address>()?
+                            .into_word(),
+                    ],
+                    "some other cp".abi_encode().into(),
+                )
+                .unwrap(),
+            },
+        };
+
+        // use handle_log instead of concrete handler to test dispatch
+        let res = handle_log(conn, log);
+
+        // checks
+        assert_eq!(
+            format!("{:?}", res.unwrap_err()),
+            "count 0 should have been 1"
+        );
+
+        Ok(())
+    }
 }
