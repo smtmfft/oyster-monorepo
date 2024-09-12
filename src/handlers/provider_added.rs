4a702f79aac159d428826b08aea2873054fe0564
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-12 17:26:47 +0530

    add test that readds the provider after it is deactivated, expected to fail since it is not handled yet

--- src/handlers/provider_added.rs
+++ src/handlers/provider_added.rs
@@ -51,6 +51,8 @@ mod tests {
 
         let contract = "0x1111111111111111111111111111111111111111".parse()?;
 
+        assert_eq!(providers::table.count().get_result(conn), Ok(0));
+
         // log under test
         let log = Log {
             block_hash: Some(keccak256!("some block").into()),
@@ -108,6 +110,16 @@ mod tests {
             ))
             .execute(conn)?;
 
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some other cp".to_owned(),
+                true
+            ))
+        );
+
         // log under test
         let log = Log {
             block_hash: Some(keccak256!("some block").into()),
@@ -137,6 +149,73 @@ mod tests {
 
         // checks
         assert_eq!(format!("{:?}", res.unwrap_err()), "failed to add provider\n\nCaused by:\n    duplicate key value violates unique constraint \"providers_pkey\"");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_add_new_provider_when_it_is_inactive() -> Result<()> {
+        // setup
+        let mut db = TestDb::new();
+        let conn = &mut db.conn;
+
+        let contract = "0x1111111111111111111111111111111111111111".parse()?;
+
+        diesel::insert_into(providers::table)
+            .values((
+                providers::id.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                providers::cp.eq("some other cp"),
+                providers::is_active.eq(false),
+            ))
+            .execute(conn)?;
+
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some other cp".to_owned(),
+                false
+            ))
+        );
+
+        // log under test
+        let log = Log {
+            block_hash: Some(keccak256!("some block").into()),
+            block_number: Some(42),
+            block_timestamp: None,
+            log_index: Some(69),
+            transaction_hash: Some(keccak256!("some tx").into()),
+            transaction_index: Some(420),
+            removed: false,
+            inner: alloy::primitives::Log {
+                address: contract,
+                data: LogData::new(
+                    vec![
+                        event!("ProviderAdded(address,string)").into(),
+                        "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"
+                            .parse::<Address>()?
+                            .into_word(),
+                    ],
+                    "some cp".abi_encode().into(),
+                )
+                .unwrap(),
+            },
+        };
+
+        // use handle_log instead of concrete handler to test dispatch
+        handle_log(conn, log)?;
+
+        // checks
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some cp".to_owned(),
+                true
+            ))
+        );
 
         Ok(())
     }
