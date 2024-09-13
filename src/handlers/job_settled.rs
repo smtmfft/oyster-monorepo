36c0754115ec51b9b48646d3374850974299c9e7
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-13 13:26:22 +0530

    add test that settles a job

--- src/handlers/job_settled.rs
+++ src/handlers/job_settled.rs
@@ -22,7 +22,7 @@ use tracing::{info, instrument};
 pub fn handle_job_settled(conn: &mut PgConnection, log: Log) -> Result<()> {
     info!(?log, "processing");
 
-    todo!()
+    todo!("handle_job_settled")
 }
 
 #[cfg(test)]
@@ -37,4 +37,112 @@ mod tests {
     use crate::schema::providers;
 
     use super::*;
+
+    #[test]
+    fn test_settle_existing_job() -> Result<()> {
+        // setup
+        let mut db = TestDb::new();
+        let conn = &mut db.conn;
+
+        let contract = "0x1111111111111111111111111111111111111111".parse()?;
+
+        diesel::insert_into(providers::table)
+            .values((
+                providers::id.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                providers::cp.eq("some cp"),
+                providers::is_active.eq(true),
+            ))
+            .execute(conn)?;
+
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some cp".to_owned(),
+                true
+            ))
+        );
+
+        let timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)?
+            .as_secs();
+        // we do this after the timestamp to truncate beyond seconds
+        let now = std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(timestamp);
+        diesel::insert_into(jobs::table)
+            .values((
+                jobs::id.eq("0x3333333333333333333333333333333333333333333333333333333333333333"),
+                jobs::owner.eq("0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"),
+                jobs::provider.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                jobs::metadata.eq("some metadata"),
+                jobs::rate.eq(1.eip712_data_word().to_string()),
+                jobs::balance.eq(20.eip712_data_word().to_string()),
+                jobs::last_settled.eq(&now),
+                jobs::created.eq(&now),
+            ))
+            .execute(conn)
+            .context("failed to create job")?;
+
+        assert_eq!(jobs::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            jobs::table.select(jobs::all_columns).first(conn),
+            Ok((
+                "0x3333333333333333333333333333333333333333333333333333333333333333".to_owned(),
+                "some metadata".to_owned(),
+                "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".to_owned(),
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                1.eip712_data_word().to_string(),
+                20.eip712_data_word().to_string(),
+                now,
+                now,
+            ))
+        );
+
+        // log under test
+        let timestamp = timestamp + 5;
+        // we do this after the timestamp to truncate beyond seconds
+        let now = std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(timestamp);
+        let log = Log {
+            block_hash: Some(keccak256!("some block").into()),
+            block_number: Some(42),
+            block_timestamp: None,
+            log_index: Some(69),
+            transaction_hash: Some(keccak256!("some tx").into()),
+            transaction_index: Some(420),
+            removed: false,
+            inner: alloy::primitives::Log {
+                address: contract,
+                data: LogData::new(
+                    vec![
+                        event!("JobSettled(bytes32,uint256,uint256)").into(),
+                        "0x3333333333333333333333333333333333333333333333333333333333333333"
+                            .parse()?,
+                    ],
+                    (5, timestamp).abi_encode().into(),
+                )
+                .unwrap(),
+            },
+        };
+
+        // use handle_log instead of concrete handler to test dispatch
+        handle_log(conn, log)?;
+
+        // checks
+        assert_eq!(jobs::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            jobs::table.select(jobs::all_columns).first(conn),
+            Ok((
+                "0x3333333333333333333333333333333333333333333333333333333333333333".to_owned(),
+                "some metadata".to_owned(),
+                "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".to_owned(),
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                1.eip712_data_word().to_string(),
+                15.eip712_data_word().to_string(),
+                now,
+                now,
+            ))
+        );
+
+        Ok(())
+    }
 }
