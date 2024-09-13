6bdd71fababa272a78d05b6b2a7830146bff325e
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-13 10:40:38 +0530

    add test that tries to create a job whose provider is inactive

--- src/handlers/job_opened.rs
+++ src/handlers/job_opened.rs
@@ -127,7 +127,7 @@ mod tests {
         diesel::insert_into(providers::table)
             .values((
                 providers::id.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
-                providers::cp.eq("some other cp"),
+                providers::cp.eq("some cp"),
                 providers::is_active.eq(true),
             ))
             .execute(conn)?;
@@ -137,7 +137,7 @@ mod tests {
             providers::table.select(providers::all_columns).first(conn),
             Ok((
                 "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
-                "some other cp".to_owned(),
+                "some cp".to_owned(),
                 true
             ))
         );
@@ -255,6 +255,82 @@ mod tests {
                             .parse::<Address>()?
                             .into_word(),
                         "0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC"
+                            .parse::<Address>()?
+                            .into_word(),
+                    ],
+                    ("some metadata", 1, 2, timestamp).abi_encode().into(),
+                )
+                .unwrap(),
+            },
+        };
+
+        // use handle_log instead of concrete handler to test dispatch
+        let res = handle_log(conn, log);
+
+        // checks
+        assert_eq!(jobs::table.count().get_result(conn), Ok(0));
+        assert_eq!(
+            format!("{:?}", res.unwrap_err()),
+            "failed to create job\n\nCaused by:\n    insert or update on table \"jobs\" violates foreign key constraint \"jobs_provider_fkey\""
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_create_job_when_provider_is_not_active() -> Result<()> {
+        // setup
+        let mut db = TestDb::new();
+        let conn = &mut db.conn;
+
+        let contract = "0x1111111111111111111111111111111111111111".parse()?;
+
+        diesel::insert_into(providers::table)
+            .values((
+                providers::id.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                providers::cp.eq("some cp"),
+                providers::is_active.eq(false),
+            ))
+            .execute(conn)?;
+
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some cp".to_owned(),
+                false,
+            ))
+        );
+
+        assert_eq!(jobs::table.count().get_result(conn), Ok(0));
+
+        // log under test
+        let timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)?
+            .as_secs();
+        // we do this after the timestamp to truncate beyond seconds
+        let now = std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(timestamp);
+        let log = Log {
+            block_hash: Some(keccak256!("some block").into()),
+            block_number: Some(42),
+            block_timestamp: None,
+            log_index: Some(69),
+            transaction_hash: Some(keccak256!("some tx").into()),
+            transaction_index: Some(420),
+            removed: false,
+            inner: alloy::primitives::Log {
+                address: contract,
+                data: LogData::new(
+                    vec![
+                        event!("JobOpened(bytes32,string,address,address,uint256,uint256,uint256)")
+                            .into(),
+                        "0x3333333333333333333333333333333333333333333333333333333333333333"
+                            .parse()?,
+                        "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"
+                            .parse::<Address>()?
+                            .into_word(),
+                        "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"
                             .parse::<Address>()?
                             .into_word(),
                     ],
