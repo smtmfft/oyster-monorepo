72333a8ce6c45168f88682a967c16e7b75c21339
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-14 16:02:29 +0530

    stub handler for JobClosed

---
+++ src/handlers/job_closed.rs
@@ -0,0 +1,23 @@
+use std::ops::Sub;
+use std::str::FromStr;
+
+use crate::schema::jobs;
+use alloy::hex::ToHexExt;
+use alloy::primitives::U256;
+use alloy::rpc::types::Log;
+use alloy::sol_types::SolValue;
+use anyhow::Context;
+use anyhow::Result;
+use bigdecimal::BigDecimal;
+use diesel::ExpressionMethods;
+use diesel::PgConnection;
+use diesel::RunQueryDsl;
+use tracing::warn;
+use tracing::{info, instrument};
+
+#[instrument(level = "info", skip_all, parent = None, fields(block = log.block_number, idx = log.log_index))]
+pub fn handle_job_closed(conn: &mut PgConnection, log: Log) -> Result<()> {
+    info!(?log, "processing");
+
+    todo!()
+}
