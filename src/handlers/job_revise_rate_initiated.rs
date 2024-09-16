68e6e0d8b09adcf2dcec50712397195eedc7b798
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-16 11:30:48 +0530

    clean up

--- src/handlers/job_revise_rate_initiated.rs
+++ src/handlers/job_revise_rate_initiated.rs
@@ -1,22 +1,10 @@
-use std::ops::Add;
-use std::str::FromStr;
-
-use crate::schema::jobs;
-use alloy::hex::ToHexExt;
-use alloy::primitives::U256;
 use alloy::rpc::types::Log;
-use alloy::sol_types::SolValue;
-use anyhow::Context;
 use anyhow::Result;
-use bigdecimal::BigDecimal;
-use diesel::ExpressionMethods;
 use diesel::PgConnection;
-use diesel::RunQueryDsl;
-use tracing::warn;
 use tracing::{info, instrument};
 
 #[instrument(level = "info", skip_all, parent = None, fields(block = log.block_number, idx = log.log_index))]
-pub fn handle_job_revise_rate_initiated(conn: &mut PgConnection, log: Log) -> Result<()> {
+pub fn handle_job_revise_rate_initiated(_conn: &mut PgConnection, log: Log) -> Result<()> {
     info!(?log, "processing");
 
     info!("empty impl, supposed to be handled by LockCreated");
