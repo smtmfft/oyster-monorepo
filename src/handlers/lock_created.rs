b5bbf4a3be56de6662b2e8daf0a0b2f49fac2ec7
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-16 14:25:04 +0530

    add test that tries to change rate when a lock already exists

--- src/handlers/lock_created.rs
+++ src/handlers/lock_created.rs
@@ -362,6 +362,246 @@ mod tests {
     }
 
     #[test]
+    fn test_rate_lock_when_it_already_exists() -> Result<()> {
+        // setup
+        let mut db = TestDb::new();
+        let conn = &mut db.conn;
+
+        let contract = "0x1111111111111111111111111111111111111111".parse()?;
+
+        diesel::insert_into(providers::table)
+            .values((
+                providers::id.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                providers::cp.eq("some cp"),
+                providers::is_active.eq(true),
+            ))
+            .execute(conn)?;
+
+        let original_timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)?
+            .as_secs();
+        // we do this after the timestamp to truncate beyond seconds
+        let original_now =
+            std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(original_timestamp);
+        diesel::insert_into(jobs::table)
+            .values((
+                jobs::id.eq("0x4444444444444444444444444444444444444444444444444444444444444444"),
+                jobs::owner.eq("0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"),
+                jobs::provider.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                jobs::metadata.eq("some other metadata"),
+                jobs::rate.eq(BigDecimal::from(3)),
+                jobs::balance.eq(BigDecimal::from(21)),
+                jobs::last_settled.eq(&original_now),
+                jobs::created.eq(&original_now),
+                jobs::is_closed.eq(false),
+            ))
+            .execute(conn)
+            .context("failed to create job")?;
+        let creation_timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)?
+            .as_secs();
+        // we do this after the timestamp to truncate beyond seconds
+        let creation_now =
+            std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(creation_timestamp);
+        diesel::insert_into(jobs::table)
+            .values((
+                jobs::id.eq("0x3333333333333333333333333333333333333333333333333333333333333333"),
+                jobs::owner.eq("0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"),
+                jobs::provider.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                jobs::metadata.eq("some metadata"),
+                jobs::rate.eq(BigDecimal::from(1)),
+                jobs::balance.eq(BigDecimal::from(20)),
+                jobs::last_settled.eq(&creation_now),
+                jobs::created.eq(&creation_now),
+                jobs::is_closed.eq(false),
+            ))
+            .execute(conn)
+            .context("failed to create job")?;
+        let revise_now = original_now.add(Duration::from_secs(300));
+        diesel::insert_into(revise_rate_requests::table)
+            .values((
+                revise_rate_requests::id
+                    .eq("0x4444444444444444444444444444444444444444444444444444444444444444"),
+                revise_rate_requests::value.eq(BigDecimal::from(2)),
+                revise_rate_requests::updates_at.eq(&revise_now),
+                revise_rate_requests::status.eq(Status::Cancelled),
+            ))
+            .execute(conn)
+            .context("failed to create revise rate request")?;
+        diesel::insert_into(revise_rate_requests::table)
+            .values((
+                revise_rate_requests::id
+                    .eq("0x3333333333333333333333333333333333333333333333333333333333333333"),
+                revise_rate_requests::value.eq(BigDecimal::from(5)),
+                revise_rate_requests::updates_at.eq(&creation_now.add(Duration::from_secs(600))),
+                revise_rate_requests::status.eq(Status::InProgress),
+            ))
+            .execute(conn)
+            .context("failed to create revise rate request")?;
+
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some cp".to_owned(),
+                true
+            ))
+        );
+
+        assert_eq!(jobs::table.count().get_result(conn), Ok(2));
+        assert_eq!(
+            jobs::table
+                .select(jobs::all_columns)
+                .order_by(jobs::id)
+                .load(conn),
+            Ok(vec![
+                (
+                    "0x3333333333333333333333333333333333333333333333333333333333333333".to_owned(),
+                    "some metadata".to_owned(),
+                    "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".to_owned(),
+                    "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                    BigDecimal::from(1),
+                    BigDecimal::from(20),
+                    creation_now,
+                    creation_now,
+                    false,
+                ),
+                (
+                    "0x4444444444444444444444444444444444444444444444444444444444444444".to_owned(),
+                    "some other metadata".to_owned(),
+                    "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".to_owned(),
+                    "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                    BigDecimal::from(3),
+                    BigDecimal::from(21),
+                    original_now,
+                    original_now,
+                    false,
+                )
+            ])
+        );
+
+        assert_eq!(revise_rate_requests::table.count().get_result(conn), Ok(2));
+        assert_eq!(
+            revise_rate_requests::table
+                .select(revise_rate_requests::all_columns)
+                .order_by(revise_rate_requests::id)
+                .load(conn),
+            Ok(vec![
+                (
+                    "0x3333333333333333333333333333333333333333333333333333333333333333".to_owned(),
+                    BigDecimal::from(5),
+                    creation_now.add(Duration::from_secs(600)),
+                    Status::InProgress,
+                ),
+                (
+                    "0x4444444444444444444444444444444444444444444444444444444444444444".to_owned(),
+                    BigDecimal::from(2),
+                    revise_now,
+                    Status::Cancelled,
+                )
+            ])
+        );
+
+        let log = Log {
+            block_hash: Some(keccak256!("some block").into()),
+            block_number: Some(42),
+            block_timestamp: None,
+            log_index: Some(69),
+            transaction_hash: Some(keccak256!("some tx").into()),
+            transaction_index: Some(420),
+            removed: false,
+            inner: alloy::primitives::Log {
+                address: contract,
+                data: LogData::new(
+                    vec![
+                        event!("LockCreated(bytes32,bytes32,uint256,uint256)").into(),
+                        RATE_LOCK_SELECTOR.into(),
+                        "0x3333333333333333333333333333333333333333333333333333333333333333"
+                            .parse()?,
+                    ],
+                    (5, creation_timestamp + 600).abi_encode_sequence().into(),
+                )
+                .unwrap(),
+            },
+        };
+
+        // use handle_log instead of concrete handler to test dispatch
+        let res = handle_log(conn, log);
+
+        // checks
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some cp".to_owned(),
+                true
+            ))
+        );
+
+        assert_eq!(jobs::table.count().get_result(conn), Ok(2));
+        assert_eq!(
+            jobs::table
+                .select(jobs::all_columns)
+                .order_by(jobs::id)
+                .load(conn),
+            Ok(vec![
+                (
+                    "0x3333333333333333333333333333333333333333333333333333333333333333".to_owned(),
+                    "some metadata".to_owned(),
+                    "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".to_owned(),
+                    "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                    BigDecimal::from(1),
+                    BigDecimal::from(20),
+                    creation_now,
+                    creation_now,
+                    false,
+                ),
+                (
+                    "0x4444444444444444444444444444444444444444444444444444444444444444".to_owned(),
+                    "some other metadata".to_owned(),
+                    "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".to_owned(),
+                    "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                    BigDecimal::from(3),
+                    BigDecimal::from(21),
+                    original_now,
+                    original_now,
+                    false,
+                )
+            ])
+        );
+
+        assert_eq!(
+            format!("{:?}", res.unwrap_err()),
+            "failed to create revise rate request\n\nCaused by:\n    duplicate key value violates unique constraint \"revise_rate_requests_pkey\""
+        );
+        assert_eq!(revise_rate_requests::table.count().get_result(conn), Ok(2));
+        assert_eq!(
+            revise_rate_requests::table
+                .select(revise_rate_requests::all_columns)
+                .order_by(revise_rate_requests::id)
+                .load(conn),
+            Ok(vec![
+                (
+                    "0x3333333333333333333333333333333333333333333333333333333333333333".to_owned(),
+                    BigDecimal::from(5),
+                    creation_now.add(Duration::from_secs(600)),
+                    Status::InProgress,
+                ),
+                (
+                    "0x4444444444444444444444444444444444444444444444444444444444444444".to_owned(),
+                    BigDecimal::from(2),
+                    revise_now,
+                    Status::Cancelled,
+                )
+            ])
+        );
+
+        Ok(())
+    }
+
+    #[test]
     fn test_other_locks() -> Result<()> {
         // setup
         let mut db = TestDb::new();
