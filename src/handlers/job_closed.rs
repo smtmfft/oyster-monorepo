e8f05155a829e4fdee20bf065a71e730cd402d1a
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-16 15:22:05 +0530

    add test that tries to close a non existent job

--- src/handlers/job_closed.rs
+++ src/handlers/job_closed.rs
@@ -219,4 +219,130 @@ mod tests {
 
         Ok(())
     }
+
+    #[test]
+    fn test_close_non_existent_job() -> Result<()> {
+        // setup
+        let mut db = TestDb::new();
+        let conn = &mut db.conn;
+
+        let contract = "0x1111111111111111111111111111111111111111".parse()?;
+
+        diesel::insert_into(providers::table)
+            .values((
+                providers::id.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                providers::cp.eq("some cp"),
+                providers::is_active.eq(true),
+            ))
+            .execute(conn)?;
+
+        let original_timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)?
+            .as_secs();
+        // we do this after the timestamp to truncate beyond seconds
+        let original_now =
+            std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(original_timestamp);
+        diesel::insert_into(jobs::table)
+            .values((
+                jobs::id.eq("0x4444444444444444444444444444444444444444444444444444444444444444"),
+                jobs::owner.eq("0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"),
+                jobs::provider.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                jobs::metadata.eq("some other metadata"),
+                jobs::rate.eq(BigDecimal::from(3)),
+                jobs::balance.eq(BigDecimal::from(21)),
+                jobs::last_settled.eq(&original_now),
+                jobs::created.eq(&original_now),
+                jobs::is_closed.eq(false),
+            ))
+            .execute(conn)
+            .context("failed to create job")?;
+
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some cp".to_owned(),
+                true
+            ))
+        );
+
+        assert_eq!(jobs::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            jobs::table
+                .select(jobs::all_columns)
+                .order_by(jobs::id)
+                .load(conn),
+            Ok(vec![(
+                "0x4444444444444444444444444444444444444444444444444444444444444444".to_owned(),
+                "some other metadata".to_owned(),
+                "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".to_owned(),
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                BigDecimal::from(3),
+                BigDecimal::from(21),
+                original_now,
+                original_now,
+                false,
+            )])
+        );
+
+        // log under test
+        let log = Log {
+            block_hash: Some(keccak256!("some block").into()),
+            block_number: Some(42),
+            block_timestamp: None,
+            log_index: Some(69),
+            transaction_hash: Some(keccak256!("some tx").into()),
+            transaction_index: Some(420),
+            removed: false,
+            inner: alloy::primitives::Log {
+                address: contract,
+                data: LogData::new(
+                    vec![
+                        event!("JobClosed(bytes32)").into(),
+                        "0x3333333333333333333333333333333333333333333333333333333333333333"
+                            .parse()?,
+                    ],
+                    Bytes::new(),
+                )
+                .unwrap(),
+            },
+        };
+
+        // use handle_log instead of concrete handler to test dispatch
+        let res = handle_log(conn, log);
+
+        // checks
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some cp".to_owned(),
+                true
+            ))
+        );
+
+        assert_eq!(format!("{:?}", res.unwrap_err()), "could not find job");
+        assert_eq!(jobs::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            jobs::table
+                .select(jobs::all_columns)
+                .order_by(jobs::id)
+                .load(conn),
+            Ok(vec![(
+                "0x4444444444444444444444444444444444444444444444444444444444444444".to_owned(),
+                "some other metadata".to_owned(),
+                "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".to_owned(),
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                BigDecimal::from(3),
+                BigDecimal::from(21),
+                original_now,
+                original_now,
+                false,
+            )])
+        );
+
+        Ok(())
+    }
 }
