d242ade06e646035657c6af77768a633aab84574
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-17 12:17:51 +0530

    basic impl that just sets rates

--- src/handlers/job_revise_rate_finalized.rs
+++ src/handlers/job_revise_rate_finalized.rs
@@ -1,11 +1,29 @@
+use std::str::FromStr;
+
+use crate::schema::jobs;
+use crate::schema::revise_rate_requests;
+use alloy::hex::ToHexExt;
+use alloy::primitives::U256;
 use alloy::rpc::types::Log;
+use alloy::sol_types::SolValue;
+use anyhow::Context;
 use anyhow::Result;
+use bigdecimal::BigDecimal;
+use diesel::sql_types::Numeric;
+use diesel::sql_types::Timestamp;
+use diesel::ExpressionMethods;
+use diesel::IntoSql;
 use diesel::PgConnection;
+use diesel::QueryDsl;
+use diesel::RunQueryDsl;
+use ethp::keccak256;
 use tracing::warn;
 use tracing::{info, instrument};
 
+static RATE_LOCK_SELECTOR: [u8; 32] = keccak256!("RATE_LOCK");
+
 #[instrument(level = "info", skip_all, parent = None, fields(block = log.block_number, idx = log.log_index))]
-pub fn handle_job_revise_rate_finalized(_conn: &mut PgConnection, log: Log) -> Result<()> {
+pub fn handle_job_revise_rate_finalized(conn: &mut PgConnection, log: Log) -> Result<()> {
     info!(?log, "processing");
 
     // while we do have enough context here to handle this properly,
@@ -13,5 +31,207 @@ pub fn handle_job_revise_rate_finalized(_conn: &mut PgConnection, log: Log) -> R
     // which also more or less handles the lock aspects of this
     // we still need to set the new rates
 
+    let id = log.topics()[1].encode_hex_with_prefix();
+    let rate = U256::abi_decode(&log.data().data, true)?;
+    let rate = BigDecimal::from_str(&rate.to_string())?;
+
+    info!(id, ?rate, "depositing into job");
+
+    diesel::update(jobs::table)
+        .filter(jobs::id.eq(&id))
+        .set(jobs::rate.eq(&rate))
+        .execute(conn)
+        .context("failed to update job")?;
+
+    info!(id, ?rate, "deposited into job");
+
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use std::ops::Add;
+    use std::time::Duration;
+
+    use alloy::{primitives::LogData, rpc::types::Log};
+    use anyhow::Result;
+    use bigdecimal::BigDecimal;
+    use diesel::QueryDsl;
+    use ethp::{event, keccak256};
+
+    use crate::handlers::handle_log;
+    use crate::handlers::test_db::TestDb;
+    use crate::schema::{jobs, providers};
+
+    use super::*;
+
+    #[test]
+    fn test_revise_rate_finalized() -> Result<()> {
+        // setup
+        let mut db = TestDb::new();
+        let conn = &mut db.conn;
+
+        let contract = "0x1111111111111111111111111111111111111111".parse()?;
+
+        diesel::insert_into(providers::table)
+            .values((
+                providers::id.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                providers::cp.eq("some cp"),
+                providers::is_active.eq(true),
+            ))
+            .execute(conn)?;
+
+        let original_timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)?
+            .as_secs();
+        // we do this after the timestamp to truncate beyond seconds
+        let original_now =
+            std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(original_timestamp);
+        diesel::insert_into(jobs::table)
+            .values((
+                jobs::id.eq("0x4444444444444444444444444444444444444444444444444444444444444444"),
+                jobs::owner.eq("0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"),
+                jobs::provider.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                jobs::metadata.eq("some other metadata"),
+                jobs::rate.eq(BigDecimal::from(3)),
+                jobs::balance.eq(BigDecimal::from(21)),
+                jobs::last_settled.eq(&original_now),
+                jobs::created.eq(&original_now),
+                jobs::is_closed.eq(false),
+            ))
+            .execute(conn)
+            .context("failed to create job")?;
+        let creation_timestamp = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)?
+            .as_secs();
+        // we do this after the timestamp to truncate beyond seconds
+        let creation_now =
+            std::time::SystemTime::UNIX_EPOCH + std::time::Duration::from_secs(creation_timestamp);
+        diesel::insert_into(jobs::table)
+            .values((
+                jobs::id.eq("0x3333333333333333333333333333333333333333333333333333333333333333"),
+                jobs::owner.eq("0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB"),
+                jobs::provider.eq("0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa"),
+                jobs::metadata.eq("some metadata"),
+                jobs::rate.eq(BigDecimal::from(1)),
+                jobs::balance.eq(BigDecimal::from(20)),
+                jobs::last_settled.eq(&creation_now),
+                jobs::created.eq(&creation_now),
+                jobs::is_closed.eq(false),
+            ))
+            .execute(conn)
+            .context("failed to create job")?;
+
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some cp".to_owned(),
+                true
+            ))
+        );
+
+        assert_eq!(jobs::table.count().get_result(conn), Ok(2));
+        assert_eq!(
+            jobs::table
+                .select(jobs::all_columns)
+                .order_by(jobs::id)
+                .load(conn),
+            Ok(vec![
+                (
+                    "0x3333333333333333333333333333333333333333333333333333333333333333".to_owned(),
+                    "some metadata".to_owned(),
+                    "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".to_owned(),
+                    "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                    BigDecimal::from(1),
+                    BigDecimal::from(20),
+                    creation_now,
+                    creation_now,
+                    false,
+                ),
+                (
+                    "0x4444444444444444444444444444444444444444444444444444444444444444".to_owned(),
+                    "some other metadata".to_owned(),
+                    "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".to_owned(),
+                    "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                    BigDecimal::from(3),
+                    BigDecimal::from(21),
+                    original_now,
+                    original_now,
+                    false,
+                )
+            ])
+        );
+
+        let log = Log {
+            block_hash: Some(keccak256!("some block").into()),
+            block_number: Some(42),
+            block_timestamp: None,
+            log_index: Some(69),
+            transaction_hash: Some(keccak256!("some tx").into()),
+            transaction_index: Some(420),
+            removed: false,
+            inner: alloy::primitives::Log {
+                address: contract,
+                data: LogData::new(
+                    vec![
+                        event!("JobReviseRateFinalized(bytes32,uint256)").into(),
+                        "0x3333333333333333333333333333333333333333333333333333333333333333"
+                            .parse()?,
+                    ],
+                    5.abi_encode().into(),
+                )
+                .unwrap(),
+            },
+        };
+
+        // use handle_log instead of concrete handler to test dispatch
+        handle_log(conn, log)?;
+
+        // checks
+        assert_eq!(providers::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            providers::table.select(providers::all_columns).first(conn),
+            Ok((
+                "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                "some cp".to_owned(),
+                true
+            ))
+        );
+
+        assert_eq!(jobs::table.count().get_result(conn), Ok(2));
+        assert_eq!(
+            jobs::table
+                .select(jobs::all_columns)
+                .order_by(jobs::id)
+                .load(conn),
+            Ok(vec![
+                (
+                    "0x3333333333333333333333333333333333333333333333333333333333333333".to_owned(),
+                    "some metadata".to_owned(),
+                    "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".to_owned(),
+                    "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                    BigDecimal::from(5),
+                    BigDecimal::from(20),
+                    creation_now,
+                    creation_now,
+                    false,
+                ),
+                (
+                    "0x4444444444444444444444444444444444444444444444444444444444444444".to_owned(),
+                    "some other metadata".to_owned(),
+                    "0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB".to_owned(),
+                    "0xaAaAaAaaAaAaAaaAaAAAAAAAAaaaAaAaAaaAaaAa".to_owned(),
+                    BigDecimal::from(3),
+                    BigDecimal::from(21),
+                    original_now,
+                    original_now,
+                    false,
+                )
+            ])
+        );
+
+        Ok(())
+    }
 }
