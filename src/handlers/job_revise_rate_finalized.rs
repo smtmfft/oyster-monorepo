46c40c53b3b34c58198612b7b3a1eade177c8e9a
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-17 12:20:48 +0530

    error out if job is not found

--- src/handlers/job_revise_rate_finalized.rs
+++ src/handlers/job_revise_rate_finalized.rs
@@ -37,11 +37,15 @@ pub fn handle_job_revise_rate_finalized(conn: &mut PgConnection, log: Log) -> Re
 
     info!(id, ?rate, "depositing into job");
 
-    diesel::update(jobs::table)
+    let count = diesel::update(jobs::table)
         .filter(jobs::id.eq(&id))
         .set(jobs::rate.eq(&rate))
         .execute(conn)
         .context("failed to update job")?;
+
+    if count != 1 {
+        return Err(anyhow::anyhow!("could not find job"));
+    }
 
     info!(id, ?rate, "deposited into job");
 
