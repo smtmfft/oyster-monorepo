f64601fec992a7c15a7e85326baf0b315889da12
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-17 12:45:31 +0530

    route logs to handler

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -118,6 +118,8 @@ pub fn handle_log(conn: &mut PgConnection, log: Log) -> Result<()> {
         handle_job_revise_rate_cancelled(conn, log)
     } else if log_type == JOB_REVISE_RATE_FINALIZED {
         handle_job_revise_rate_finalized(conn, log)
+    } else if log_type == JOB_METADATA_UPDATED {
+        handle_job_metadata_updated(conn, log)
     } else if log_type == LOCK_CREATED {
         handle_lock_created(conn, log)
     } else if log_type == LOCK_DELETED {
