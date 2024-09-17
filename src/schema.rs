c195e1bcf32717bd344886b1b562f10f3aa683f5
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-09-17 10:37:27 +0530

    update schema

--- src/schema.rs
+++ src/schema.rs
@@ -1,11 +1,5 @@
 // @generated automatically by Diesel CLI.
 
-pub mod sql_types {
-    #[derive(diesel::query_builder::QueryId, Clone, diesel::sql_types::SqlType)]
-    #[diesel(postgres_type(name = "request_status"))]
-    pub struct RequestStatus;
-}
-
 diesel::table! {
     jobs (id) {
         #[max_length = 66]
@@ -33,15 +27,11 @@ diesel::table! {
 }
 
 diesel::table! {
-    use diesel::sql_types::*;
-    use super::sql_types::RequestStatus;
-
     revise_rate_requests (id) {
         #[max_length = 66]
         id -> Bpchar,
         value -> Numeric,
         updates_at -> Timestamp,
-        status -> RequestStatus,
     }
 }
 
