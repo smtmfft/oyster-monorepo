fb517c727bf105d6dd964e3d8a9dcd106a4a1a5c
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-10-04 10:54:58 +0530

    update schema

--- src/schema.rs
+++ src/schema.rs
@@ -41,7 +41,6 @@ diesel::table! {
     }
 }
 
-diesel::joinable!(jobs -> providers (provider));
 diesel::joinable!(revise_rate_requests -> jobs (id));
 
 diesel::allow_tables_to_appear_in_same_query!(
