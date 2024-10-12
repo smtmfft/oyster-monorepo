28b5b8340ca633cec5f4c0e4fc849143d60c2e39
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-10-12 16:29:57 +0530

    read attestation in the guest

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -4,7 +4,9 @@ fn main() {
     // TODO: Implement your guest code here
 
     // read the input
-    let input: u32 = env::read();
+    let input: Vec<u8> = env::read();
+
+    println!("Input len: {}", input.len());
 
     // TODO: do something with the input
 
