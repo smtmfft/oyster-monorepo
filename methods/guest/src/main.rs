3d8d00fa37031326f22d3036e3d388dc67353037
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-10-12 16:43:56 +0530

    clean up

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -3,15 +3,11 @@ use risc0_zkvm::guest::env;
 use std::io::Read;
 
 fn main() {
-    // TODO: Implement your guest code here
-
     // read the attestation
     let mut attestation = Vec::<u8>::new();
     env::stdin().read_to_end(&mut attestation).unwrap();
 
     println!("Input len: {}", attestation.len());
-
-    // TODO: do something with the input
 
     // write public output to the journal
     env::commit(&attestation.len());
