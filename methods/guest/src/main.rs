991c5b704edb3002fca62c3732095f6049c4144a
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-10-15 17:54:42 +0530

    dynamic module id offset

--- methods/guest/src/main.rs
+++ methods/guest/src/main.rs
@@ -57,80 +57,98 @@ fn main() {
             0x69, // text of size 9, "module_id" key
         ]
     );
-    assert_eq!(
-        attestation[21..23],
-        [
-            0x78, // text with one byte length follows
-            0x27, // text of size 39, module id value
-        ]
-    );
-    assert_eq!(attestation[62], 0x66); // text of size 6, "digest" key
-    assert_eq!(attestation[69], 0x66); // text of size 6, "SHA384" value
+    assert_eq!(attestation[21], 0x78); // text with one byte length follows
+
+    // skip to after the module id
+    let mut offset = 23 + attestation[22] as usize;
+
+    assert_eq!(attestation[offset], 0x66); // text of size 6, "digest" key
+    assert_eq!(attestation[offset + 7], 0x66); // text of size 6, "SHA384" value
 
     // assert timestamp key
-    assert_eq!(attestation[76], 0x69); // text of size 9
-    assert_eq!(&attestation[77..86], b"timestamp");
+    assert_eq!(attestation[offset + 14], 0x69); // text of size 9
+    assert_eq!(&attestation[offset + 15..offset + 24], b"timestamp");
     // commit the timestamp value
-    assert_eq!(attestation[86], 0x1b); // unsigned int, size 8
-    println!("Timestamp: {:?}", &attestation[87..95]);
-    env::commit_slice(&attestation[87..95]);
+    assert_eq!(attestation[offset + 24], 0x1b); // unsigned int, size 8
+    println!("Timestamp: {:?}", &attestation[offset + 25..offset + 33]);
+    env::commit_slice(&attestation[offset + 25..offset + 33]);
 
     // assert pcrs key
-    assert_eq!(attestation[95], 0x64); // text of size 4
-    assert_eq!(&attestation[96..100], b"pcrs");
+    assert_eq!(attestation[offset + 33], 0x64); // text of size 4
+    assert_eq!(&attestation[offset + 34..offset + 38], b"pcrs");
     // commit pcrs 0, 1 and 2
-    assert_eq!(attestation[100], 0xb0); // pcrs is a map of size 16
+    assert_eq!(attestation[offset + 38], 0xb0); // pcrs is a map of size 16
+
+    offset += 39;
     assert_eq!(
-        attestation[101..104],
+        attestation[offset..offset + 3],
         [
             0x00, // pcr number
             0x58, // bytes with one byte length follows
             0x30, // 48 length
         ]
     );
-    println!("PCR0: {:?}", &attestation[104..152]);
-    env::commit_slice(&attestation[104..152]);
-    assert_eq!(attestation[152..155], [0x01, 0x58, 0x30]);
-    println!("PCR1: {:?}", &attestation[155..203]);
-    env::commit_slice(&attestation[155..203]);
-    assert_eq!(attestation[203..206], [0x02, 0x58, 0x30]);
-    println!("PCR2: {:?}", &attestation[206..254]);
-    env::commit_slice(&attestation[206..254]);
+    println!("PCR0: {:?}", &attestation[offset + 3..offset + 51]);
+    env::commit_slice(&attestation[offset + 3..offset + 51]);
+
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x01, 0x58, 0x30]);
+    println!("PCR1: {:?}", &attestation[offset + 3..offset + 51]);
+    env::commit_slice(&attestation[offset + 3..offset + 51]);
+
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x02, 0x58, 0x30]);
+    println!("PCR2: {:?}", &attestation[offset + 3..offset + 51]);
+    env::commit_slice(&attestation[offset + 3..offset + 51]);
 
     // skip rest of the pcrs, 3 to 15
-    assert_eq!(attestation[254..257], [0x03, 0x58, 0x30]);
-    assert_eq!(attestation[305..308], [0x04, 0x58, 0x30]);
-    assert_eq!(attestation[356..359], [0x05, 0x58, 0x30]);
-    assert_eq!(attestation[407..410], [0x06, 0x58, 0x30]);
-    assert_eq!(attestation[458..461], [0x07, 0x58, 0x30]);
-    assert_eq!(attestation[509..512], [0x08, 0x58, 0x30]);
-    assert_eq!(attestation[560..563], [0x09, 0x58, 0x30]);
-    assert_eq!(attestation[611..614], [0x0a, 0x58, 0x30]);
-    assert_eq!(attestation[662..665], [0x0b, 0x58, 0x30]);
-    assert_eq!(attestation[713..716], [0x0c, 0x58, 0x30]);
-    assert_eq!(attestation[764..767], [0x0d, 0x58, 0x30]);
-    assert_eq!(attestation[815..818], [0x0e, 0x58, 0x30]);
-    assert_eq!(attestation[866..869], [0x0f, 0x58, 0x30]);
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x03, 0x58, 0x30]);
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x04, 0x58, 0x30]);
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x05, 0x58, 0x30]);
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x06, 0x58, 0x30]);
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x07, 0x58, 0x30]);
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x08, 0x58, 0x30]);
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x09, 0x58, 0x30]);
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x0a, 0x58, 0x30]);
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x0b, 0x58, 0x30]);
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x0c, 0x58, 0x30]);
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x0d, 0x58, 0x30]);
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x0e, 0x58, 0x30]);
+    offset += 51;
+    assert_eq!(attestation[offset..offset + 3], [0x0f, 0x58, 0x30]);
+    offset += 51;
     println!("Skipped rest of the pcrs");
 
     // assert certificate key
-    assert_eq!(attestation[917], 0x6b); // text of size 11
-    assert_eq!(&attestation[918..929], b"certificate");
+    assert_eq!(attestation[offset], 0x6b); // text of size 11
+    assert_eq!(&attestation[offset + 1..offset + 12], b"certificate");
     // get leaf certificate
-    assert_eq!(attestation[929], 0x59); // bytes where two byte length follows
-    let leaf_cert_size = u16::from_be_bytes([attestation[930], attestation[931]]) as usize;
+    assert_eq!(attestation[offset + 12], 0x59); // bytes where two byte length follows
+    let leaf_cert_size =
+        u16::from_be_bytes([attestation[offset + 13], attestation[offset + 14]]) as usize;
     let leaf_cert =
-        x509_cert::Certificate::from_der(&attestation[932..932 + leaf_cert_size]).unwrap();
+        x509_cert::Certificate::from_der(&attestation[offset + 15..offset + 15 + leaf_cert_size])
+            .unwrap();
+    offset += 15 + leaf_cert_size;
 
     // assert cabundle key
-    assert_eq!(attestation[932 + leaf_cert_size], 0x68); // text of length 8
-    assert_eq!(
-        &attestation[932 + leaf_cert_size + 1..932 + leaf_cert_size + 9],
-        b"cabundle"
-    );
+    assert_eq!(attestation[offset], 0x68); // text of length 8
+    assert_eq!(&attestation[offset + 1..offset + 9], b"cabundle");
 
     // cabundle should be an array, read length
-    let chain_size = attestation[932 + leaf_cert_size + 9];
+    let chain_size = attestation[offset + 9];
     // just restrict chain size instead of figuring out parsing too much
     // works for tiny field encoded cabundle up to 16 length
     assert!(chain_size > 0x80 && chain_size <= 0x90);
@@ -141,22 +159,17 @@ fn main() {
     // last certificate in the list signs the leaf certificate obtained above
 
     // track start of each section so we know where to proceed from after the block
-    let mut next_cert_start = 932 + leaf_cert_size + 10;
+    offset = offset + 10;
     {
         // start with the root cert
 
         // parse root cert
-        assert_eq!(attestation[next_cert_start], 0x59); // bytes where two byte length follows
-        let size = u16::from_be_bytes([
-            attestation[next_cert_start + 1],
-            attestation[next_cert_start + 2],
-        ]) as usize;
+        assert_eq!(attestation[offset], 0x59); // bytes where two byte length follows
+        let size = u16::from_be_bytes([attestation[offset + 1], attestation[offset + 2]]) as usize;
 
         // cert with the pubkey, start with the root
-        let mut parent_cert = x509_cert::Certificate::from_der(
-            &attestation[next_cert_start + 3..next_cert_start + 3 + size],
-        )
-        .unwrap();
+        let mut parent_cert =
+            x509_cert::Certificate::from_der(&attestation[offset + 3..offset + 3 + size]).unwrap();
 
         // commit the root pubkey
         let pubkey = parent_cert
@@ -172,21 +185,18 @@ fn main() {
         env::commit_slice(pubkey);
 
         // start of next cert that is to be verified
-        next_cert_start = next_cert_start + 3 + size;
+        offset = offset + 3 + size;
 
         for _ in 0..chain_size - 1 {
             // parse child cert
-            assert_eq!(attestation[next_cert_start], 0x59); // bytes where two byte length follows
-            let size = u16::from_be_bytes([
-                attestation[next_cert_start + 1],
-                attestation[next_cert_start + 2],
-            ]) as usize;
+            assert_eq!(attestation[offset], 0x59); // bytes where two byte length follows
+            let size =
+                u16::from_be_bytes([attestation[offset + 1], attestation[offset + 2]]) as usize;
 
             // parse the next cert and get the public key
-            let child_cert = x509_cert::Certificate::from_der(
-                &attestation[next_cert_start + 3..next_cert_start + 3 + size],
-            )
-            .unwrap();
+            let child_cert =
+                x509_cert::Certificate::from_der(&attestation[offset + 3..offset + 3 + size])
+                    .unwrap();
 
             // verify signature
             // TODO: the tbs cert is already available in DER form in the attestation, use that
@@ -202,7 +212,7 @@ fn main() {
 
             // set up for next iteration
             parent_cert = child_cert;
-            next_cert_start = next_cert_start + 3 + size;
+            offset = offset + 3 + size;
         }
 
         // verify the leaf cert with the last cert in the chain
@@ -219,49 +229,34 @@ fn main() {
     }
 
     // assert public_key key
-    assert_eq!(attestation[next_cert_start], 0x6a); // text of size 10
-    assert_eq!(
-        &attestation[next_cert_start + 1..next_cert_start + 11],
-        b"public_key"
-    );
+    assert_eq!(attestation[offset], 0x6a); // text of size 10
+    assert_eq!(&attestation[offset + 1..offset + 11], b"public_key");
     // commit public key, expected length of 64 since it is a secp256k1 key
-    assert_eq!(attestation[next_cert_start + 11], 0x58); // bytes where one byte length follows
-    assert_eq!(attestation[next_cert_start + 12], 0x40); // 64 length
-    env::commit_slice(&attestation[next_cert_start + 13..next_cert_start + 77]);
+    assert_eq!(attestation[offset + 11], 0x58); // bytes where one byte length follows
+    assert_eq!(attestation[offset + 12], 0x40); // 64 length
+    env::commit_slice(&attestation[offset + 13..offset + 77]);
 
     // assert user_data key
-    assert_eq!(attestation[next_cert_start + 77], 0x69); // text of size 9
-    assert_eq!(
-        &attestation[next_cert_start + 78..next_cert_start + 87],
-        b"user_data"
-    );
+    assert_eq!(attestation[offset + 77], 0x69); // text of size 9
+    assert_eq!(&attestation[offset + 78..offset + 87], b"user_data");
     // commit user data
     // handle cases up to 65536 size
-    let (user_data_size, user_data) = if attestation[next_cert_start + 87] == 0xf6 {
+    let (user_data_size, user_data) = if attestation[offset + 87] == 0xf6 {
         // empty
         (0, [].as_slice())
-    } else if attestation[next_cert_start + 87] == 0x58 {
+    } else if attestation[offset + 87] == 0x58 {
         // one byte length follows
-        let size = attestation[next_cert_start + 88] as u16;
+        let size = attestation[offset + 88] as u16;
 
-        (
-            size,
-            &attestation[next_cert_start + 89..next_cert_start + 89 + size as usize],
-        )
+        (size, &attestation[offset + 89..offset + 89 + size as usize])
     } else {
         // only allow 2 byte lengths as max
         // technically, this is already enforced by COSE doc size parsing
-        assert_eq!(attestation[next_cert_start + 87], 0x59);
+        assert_eq!(attestation[offset + 87], 0x59);
 
-        let size = u16::from_be_bytes([
-            attestation[next_cert_start + 88],
-            attestation[next_cert_start + 89],
-        ]);
+        let size = u16::from_be_bytes([attestation[offset + 88], attestation[offset + 89]]);
 
-        (
-            size,
-            &attestation[next_cert_start + 90..next_cert_start + 90 + size as usize],
-        )
+        (size, &attestation[offset + 90..offset + 90 + size as usize])
     };
     println!("User data: {} bytes: {:?}", user_data_size, user_data);
     // commit 2 byte length, then data
