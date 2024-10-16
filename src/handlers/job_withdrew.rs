e45ed327c28390aa6e8fed82e2a8a30a36278256
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-10-16 16:27:50 +0530

    add transaction table checks to test

--- src/handlers/job_withdrew.rs
+++ src/handlers/job_withdrew.rs
@@ -520,6 +520,18 @@ mod tests {
             .execute(conn)
             .context("failed to create job")?;
 
+        diesel::insert_into(transactions::table)
+            .values((
+                transactions::block.eq(123),
+                transactions::idx.eq(5),
+                transactions::job
+                    .eq("0x3333333333333333333333333333333333333333333333333333333333333333"),
+                transactions::amount.eq(BigDecimal::from(10)),
+                transactions::is_deposit.eq(true),
+            ))
+            .execute(conn)
+            .context("failed to create job")?;
+
         assert_eq!(providers::table.count().get_result(conn), Ok(1));
         assert_eq!(
             providers::table.select(providers::all_columns).first(conn),
@@ -560,6 +572,20 @@ mod tests {
                     false,
                 )
             ])
+        );
+
+        assert_eq!(transactions::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            transactions::table
+                .select(transactions::all_columns)
+                .first(conn),
+            Ok((
+                123i64,
+                5i64,
+                "0x3333333333333333333333333333333333333333333333333333333333333333".to_owned(),
+                BigDecimal::from(10),
+                true,
+            ))
         );
 
         let log = Log {
@@ -632,6 +658,20 @@ mod tests {
                     false,
                 )
             ])
+        );
+
+        assert_eq!(transactions::table.count().get_result(conn), Ok(1));
+        assert_eq!(
+            transactions::table
+                .select(transactions::all_columns)
+                .first(conn),
+            Ok((
+                123i64,
+                5i64,
+                "0x3333333333333333333333333333333333333333333333333333333333333333".to_owned(),
+                BigDecimal::from(10),
+                true,
+            ))
         );
 
         Ok(())
