076429d652d9fb3593913b5b84d9e47be7059169
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-10-16 16:00:28 +0530

    update schema

--- src/schema.rs
+++ src/schema.rs
@@ -41,11 +41,24 @@ diesel::table! {
     }
 }
 
+diesel::table! {
+    transactions (block, idx) {
+        block -> Int8,
+        idx -> Int8,
+        #[max_length = 66]
+        job -> Nullable<Bpchar>,
+        amount -> Numeric,
+        is_deposit -> Bool,
+    }
+}
+
 diesel::joinable!(revise_rate_requests -> jobs (id));
+diesel::joinable!(transactions -> jobs (job));
 
 diesel::allow_tables_to_appear_in_same_query!(
     jobs,
     providers,
     revise_rate_requests,
     sync,
+    transactions,
 );
