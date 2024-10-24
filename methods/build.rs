751b3cc2a4f5df8b46132dffcc276a63f77ef5cc
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-10-24 18:35:40 +0530

    use docker for reproducible builds

--- methods/build.rs
+++ methods/build.rs
@@ -1,3 +1,18 @@
+use std::collections::HashMap;
+use std::path::PathBuf;
+
+use risc0_build::{DockerOptions, GuestOptions};
+
 fn main() {
-    risc0_build::embed_methods();
+    let mut options = HashMap::new();
+    options.insert(
+        "guest",
+        GuestOptions {
+            features: vec![],
+            use_docker: Some(DockerOptions {
+                root_dir: std::env::var_os("OUT_DIR/docker/").map(PathBuf::from),
+            }),
+        },
+    );
+    risc0_build::embed_methods_with_options(options);
 }
