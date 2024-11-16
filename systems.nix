559680c2252524957bb03c85f239a22d2f65bbd0
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-11-16 07:51:01 +0530

    _: define target systems

---
+++ systems.nix
@@ -0,0 +1,24 @@
+{
+  systems = [
+    {
+      system = "x86_64-linux";
+      rust_target = "x86_64-unknown-linux-musl";
+      musl = true;
+    }
+    {
+      system = "aarch64-linux";
+      rust_target = "aarch64-unknown-linux-musl";
+      musl = true;
+    }
+    {
+      system = "x86_64-darwin";
+      rust_target = "x86_64-apple-darwin";
+      musl = false;
+    }
+    {
+      system = "aarch64-darwin";
+      rust_target = "aarch64-apple-darwin";
+      musl = false;
+    }
+  ];
+}
