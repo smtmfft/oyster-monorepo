563eda1eb0dab7aa15205db1ba326e112eb5d7a5
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-11-16 09:45:05 +0530

    _: add networking/tcp-proxy to the root flake

--- flake.nix
+++ flake.nix
@@ -24,6 +24,9 @@
       attestation-server = import ./attestation/server {
         inherit nixpkgs systemConfig fenix naersk;
       };
+      networking-tcp-proxy = import ./networking/tcp-proxy {
+        inherit nixpkgs systemConfig fenix naersk;
+      };
     });
   };
 }
