c60a510dc3ed8c2d33729c91ec41f1930341bc73
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-11-20 13:44:59 +0530

    switch the base nixpkgs to the 24.05 tag instead of the nixos branch

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
