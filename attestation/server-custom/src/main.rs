3b37d220d89f7914a9831e6519053587e1d912c4
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-12-06 11:04:58 +0530

    remove pub_key cli arg and clean up

--- attestation/server-custom/src/main.rs
+++ attestation/server-custom/src/main.rs
@@ -97,20 +97,11 @@ struct Cli {
     /// ip address of the server (e.g. 127.0.0.1:1300)
     #[arg(short, long)]
     ip_addr: String,
-
-    /// path to public key file (e.g. /app/id.pub)
-    #[arg(short, long)]
-    pub_key: String,
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::parse();
-
-    // leak in order to get a static slice
-    // okay to do since it will get cleaned up on exit
-    let pub_key = std::fs::read(cli.pub_key)?.leak::<'static>();
-    println!("pub key: {:02x?}", pub_key);
 
     let app = Router::new()
         .route("/attestation/raw", get(handle_raw))
