fa6c0f98ebe7b17b2baca75198c700f3d3266d3e
Roshan Raghupathy <thisisroshansmail@gmail.com>
2024-12-06 10:47:25 +0530

    add custom attestation server copied from the main server

---
+++ attestation/server-custom/default.nix
@@ -0,0 +1,43 @@
+{
+  nixpkgs,
+  systemConfig,
+  fenix,
+  naersk,
+}: let
+  system = systemConfig.system;
+  pkgs = nixpkgs.legacyPackages."${system}";
+  target = systemConfig.rust_target;
+  toolchain = with fenix.packages.${system};
+    combine [
+      stable.cargo
+      stable.rustc
+      targets.${target}.stable.rust-std
+    ];
+  naersk' = naersk.lib.${system}.override {
+    cargo = toolchain;
+    rustc = toolchain;
+  };
+  cc =
+    if systemConfig.static
+    then pkgs.pkgsStatic.stdenv.cc
+    else pkgs.stdenv.cc;
+in rec {
+  uncompressed = naersk'.buildPackage {
+    src = ./.;
+    CARGO_BUILD_TARGET = target;
+    TARGET_CC = "${cc}/bin/${cc.targetPrefix}cc";
+    nativeBuildInputs = [cc];
+  };
+
+  compressed =
+    pkgs.runCommand "compressed" {
+      nativeBuildInputs = [pkgs.upx];
+    } ''
+      mkdir -p $out/bin
+      cp ${uncompressed}/bin/* $out/bin/
+      chmod +w $out/bin/*
+      upx $out/bin/*
+    '';
+
+  default = compressed;
+}
